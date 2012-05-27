a45140ce20e2f023001d6d06c5ccf01529a107ad
markus <markus@c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d>
2012-05-27 18:31:40 +0000

    restruct
    git-svn-id: https://svn.spraakdata.gu.se/clt/bnfc@292 c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d

--- testing/unit-testing/testsuite.bash
+++ testing/unit-testing/testsuite.bash
@@ -1,22 +1,22 @@
 #!/bin/bash 
 
-function bnfc_haskell_parse_test {
+function haskell_parse_test {
  mkdir -p tmp; cd tmp
  cp ../input/$1/$1.cf .
  bnfc -m $1.cf > /dev/null
  make 2>1 > /dev/null
  Run=`cat ../input/$1/$1.test | ./Test$1`
- ExpectedOutput=`cat ../output/haskell/$1.out`
+ ExpectedOutput=`cat ../output/haskell/$1_parse.out`
  cd .. ; rm -rf tmp
  assertEquals "${ExpectedOutput}" "${Run}"
 }
 
-test_lbnf_haskell_parse_test(){
-    bnfc_haskell_parse_test lbnf
+test_haskell_lbnf_parse_test(){
+    haskell_parse_test lbnf
 }
 
-test_gf_haskell_parse_test(){
-    bnfc_haskell_parse_test gf
+test_haskell_gf_parse_test(){
+    haskell_parse_test gf
 }
 
 . ./shunit2
