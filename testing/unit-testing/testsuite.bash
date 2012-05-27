ba078075e246a7a2da91c72496eb67c56c2ba35c
markus <markus@c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d>
2012-05-27 18:29:28 +0000

    restruct
    git-svn-id: https://svn.spraakdata.gu.se/clt/bnfc@291 c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d

--- testing/unit-testing/testsuite.bash
+++ testing/unit-testing/testsuite.bash
@@ -6,7 +6,7 @@ function bnfc_haskell_parse_test {
  bnfc -m $1.cf > /dev/null
  make 2>1 > /dev/null
  Run=`cat ../input/$1/$1.test | ./Test$1`
- ExpectedOutput=`cat ../haskell/output/$1.out`
+ ExpectedOutput=`cat ../output/haskell/$1.out`
  cd .. ; rm -rf tmp
  assertEquals "${ExpectedOutput}" "${Run}"
 }
