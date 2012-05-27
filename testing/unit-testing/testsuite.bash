1a2265213c0f2429c687b95dacba3354bfac4282
markus <markus@c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d>
2012-05-27 19:00:54 +0000

    c unit testing
    git-svn-id: https://svn.spraakdata.gu.se/clt/bnfc@294 c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d

--- testing/unit-testing/testsuite.bash
+++ testing/unit-testing/testsuite.bash
@@ -3,20 +3,32 @@
 function parse_test {
  mkdir -p tmp; cd tmp
  cp ../input/$1/$1.cf .
- bnfc -m $1.cf > /dev/null
+ if [ $2 == "haskell" ]; then
+     bnfc -m $1.cf > /dev/null
+ elif [ $2 == "c" ]; then
+     bnfc -c -m $1.cf > /dev/null
+ fi
  make 2>1 > /dev/null
  Run=`cat ../input/$1/$1.test | ./Test$1`
- ExpectedOutput=`cat ../output/haskell/$1_parse.out`
+ ExpectedOutput=`cat ../output/$2/$1_parse.out`
  cd .. ; rm -rf tmp
  assertEquals "${ExpectedOutput}" "${Run}"
 }
 
 test_haskell_lbnf_parse_test(){
-    parse_test lbnf
+    parse_test lbnf haskell
 }
 
 test_haskell_gf_parse_test(){
-    parse_test gf
+    parse_test gf haskell
+}
+
+test_c_lbnf_parse_test(){
+    parse_test lbnf c
+}
+
+test_c_gf_parse_test(){
+    parse_test gf c
 }
 
 . ./shunit2
