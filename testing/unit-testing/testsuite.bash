76d21caf16e03521f89da96cdc65c8b02916aaa1
markus <markus@c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d>
2012-05-30 09:13:23 +0000

    more tests
    git-svn-id: https://svn.spraakdata.gu.se/clt/bnfc@295 c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d

--- testing/unit-testing/testsuite.bash
+++ testing/unit-testing/testsuite.bash
@@ -4,11 +4,20 @@ function parse_test {
  mkdir -p tmp; cd tmp
  cp ../input/$1/$1.cf .
  if [ $2 == "haskell" ]; then
+     echo " bnfc -m $1.cf"
      bnfc -m $1.cf > /dev/null
  elif [ $2 == "c" ]; then
+     echo " bnfc -c -m $1.cf"
      bnfc -c -m $1.cf > /dev/null
+ elif [ $2 == "cpp" ]; then
+     echo " bnfc -cpp -m $1.cf"
+     bnfc -cpp -m $1.cf > /dev/null
+ elif [ $2 == "ocaml" ]; then
+     echo " bnfc -ocaml -m $1.cf"
+     bnfc -ocaml -m $1.cf > /dev/null
  fi
  make 2>1 > /dev/null
+ echo " cat ../input/$1/$1.test | ./Test$1"
  Run=`cat ../input/$1/$1.test | ./Test$1`
  ExpectedOutput=`cat ../output/$2/$1_parse.out`
  cd .. ; rm -rf tmp
@@ -30,5 +39,22 @@ test_c_lbnf_parse_test(){
 test_c_gf_parse_test(){
     parse_test gf c
 }
+
+# test_cpp_lbnf_parse_test(){
+#    parse_test lbnf cpp
+#}
+
+#test_cpp_gf_parse_test(){
+#    parse_test gf cpp
+#}
+
+test_ocaml_lbnf_parse_test(){
+    parse_test lbnf ocaml
+}
+
+test_ocaml_gf_parse_test(){
+    parse_test gf ocaml
+}
+
 
 . ./shunit2
