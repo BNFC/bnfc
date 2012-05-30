3744c9bc703f3eb3abd46ab46437b2ed92645032
markus <markus@c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d>
2012-05-30 09:40:35 +0000

    more tests
    git-svn-id: https://svn.spraakdata.gu.se/clt/bnfc@296 c65aaa6c-c3b3-4e7b-ac5e-b48955d7ee5d

--- testing/unit-testing/testsuite.bash
+++ testing/unit-testing/testsuite.bash
@@ -1,27 +1,26 @@
 #!/bin/bash 
 
 function parse_test {
- mkdir -p tmp; cd tmp
- cp ../input/$1/$1.cf .
- if [ $2 == "haskell" ]; then
-     echo " bnfc -m $1.cf"
-     bnfc -m $1.cf > /dev/null
- elif [ $2 == "c" ]; then
-     echo " bnfc -c -m $1.cf"
-     bnfc -c -m $1.cf > /dev/null
- elif [ $2 == "cpp" ]; then
-     echo " bnfc -cpp -m $1.cf"
-     bnfc -cpp -m $1.cf > /dev/null
- elif [ $2 == "ocaml" ]; then
-     echo " bnfc -ocaml -m $1.cf"
-     bnfc -ocaml -m $1.cf > /dev/null
- fi
- make 2>1 > /dev/null
- echo " cat ../input/$1/$1.test | ./Test$1"
- Run=`cat ../input/$1/$1.test | ./Test$1`
- ExpectedOutput=`cat ../output/$2/$1_parse.out`
- cd .. ; rm -rf tmp
- assertEquals "${ExpectedOutput}" "${Run}"
+    mkdir -p tmp; cd tmp
+    cp ../input/$1/$1.cf .
+    if [ $2 == "haskell" ]; then
+	echo " bnfc -m $1.cf"
+	bnfc -m $1.cf > /dev/null
+    else
+	echo " bnfc -$2 -m $1.cf"
+	bnfc -$2 -m $1.cf > /dev/null
+    fi
+    make 2>1 > /dev/null
+    if  [ $2 == "java" ]; then
+	echo " cat ../input/$1/$1.test | java $1/Test"
+	Run=`cat ../input/$1/$1.test | java $1/Test`
+    else
+	echo " cat ../input/$1/$1.test | ./Test$1"
+	Run=`cat ../input/$1/$1.test | ./Test$1`
+    fi
+    ExpectedOutput=`cat ../output/$2/$1_parse.out`
+    cd .. ; rm -rf tmp
+    assertEquals "${ExpectedOutput}" "${Run}"
 }
 
 test_haskell_lbnf_parse_test(){
@@ -47,6 +46,14 @@ test_c_gf_parse_test(){
 #test_cpp_gf_parse_test(){
 #    parse_test gf cpp
 #}
+
+test_java_lbnf_parse_test(){
+    parse_test lbnf java
+}
+
+# test_java_gf_parse_test(){
+#    parse_test gf java
+# }
 
 test_ocaml_lbnf_parse_test(){
     parse_test lbnf ocaml
