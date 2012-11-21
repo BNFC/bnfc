dbfd912a6cfbb08495b8f86be4c2cfb4325f71c7
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2012-11-21 16:56:40 +0100

    remove useless code

--- source/runtime/Parsing/Chart.hs
+++ source/runtime/Parsing/Chart.hs
@@ -11,18 +11,8 @@ import Control.Monad(join)
 
 import Data.List (splitAt)
 import Algebra.RingUtils
-import Data.Matrix
 import qualified Data.Matrix.Quad as Q
 import Data.Matrix.Class
-import qualified Data.Matrix.ValiantPosition as VP
-
--- interface to charts and generic code on top of it.
-
-class IsChart c where
-   single :: AbelianGroupZ a => Pair a -> c a
---    merging  :: RingP a => Bool -> c a -> c a -> c a
-   merging' :: RingP a => Bool -> c a -> Pair a -> c a -> c a
-   root :: AbelianGroupZ a => c a -> a
 
 fingerprint = Q.fingerprint
 
@@ -55,8 +45,6 @@ mkTree = mkTree2 False -- mkTreeHelp [False,True]
 mkTree' = mkTree2 True -- mkTreeHelp [True,False]  
 
 
-
-
 type Set a = [a]
 
 -- Sets form an abelian group
@@ -67,27 +55,5 @@ instance AbelianGroup (Set a) where
 instance AbelianGroupZ (Set a) where
     isZero = null
 
-
-
--- instance IsChart MT where
---     root x = at (countColumns x - 1) 0 x
---     merging _ x y = V.merge x y
---     single (x :/: y) = quad z (singleton (x + y)) z z 
---        where z = singleton zero
--- This instance needs the Ring constraint
-
-
 type MT2 a = Q.Q a
 
--- instance IsChart MT2 where
---     root x = at (countColumns x - 1) 0 x
---     merging = VP.merge
---     merging' a c b = VP.merge' a c b
---     single x = quad z s z z 
---        where z = singleton zero
---              s = O $ singleton (leftOf x) :/: singleton (rightOf x)
-
-instance IsChart Q.SomeTri where
-    root = Q.root 
-    merging' a c b = Q.mergein a c b
-    single x = Q.single x
