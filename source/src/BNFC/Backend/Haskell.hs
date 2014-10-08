ab413a52a1e424eadad775e966866cd3dba5fd9f
Grégoire Détrez <gregoire.detrez@gu.se>
2014-10-08 16:08:05 +0200

    Use the same test program for Haskell with and without gadt

--- source/src/BNFC/Backend/Haskell.hs
+++ source/src/BNFC/Backend/Haskell.hs
@@ -17,7 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 -}
 
-module BNFC.Backend.Haskell (makeHaskell, AlexVersion(..), makefile) where
+module BNFC.Backend.Haskell (makeHaskell, AlexVersion(..), makefile, testfile) where
 
 
 
