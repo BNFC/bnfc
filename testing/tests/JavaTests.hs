08a277d95981e2f32bb3a0e5e88cc81a60dae6a4
Grégoire Détrez <gregoire.detrez@gu.se>
2014-11-14 14:51:11 +0100

    Add test for issue #31

--- testing/tests/JavaTests.hs
+++ testing/tests/JavaTests.hs
@@ -6,7 +6,7 @@ import Prelude hiding (FilePath, all)
 import Filesystem.Path (filename, basename)
 import TestData (exampleGrammars)
 
-all = makeTestSuite "Java backend" [testVisitorSkels]
+all = makeTestSuite "Java backend" [testVisitorSkels, issue31]
 
 -- | Given the path to a grammar file, this will generate java code using bnfc
 -- and make sure that the generated Visitor skeleton compiles with javac.
@@ -24,3 +24,11 @@ testVisitorSkel grammar = makeShellyTest (pathToString (basename grammar)) $
 -- | Instanciate the test above with all test grammars
 testVisitorSkels = makeTestSuite "Compile VisitSkel.java" $
     map (testVisitorSkel . fst) exampleGrammars
+
+-- | Issue
+issue31 :: Test
+issue31 = makeShellyTest "#31 Problem with multiples `rules` declaration with a common prefix" $
+    withTmpDir $ \tmp -> do
+        cp "regression-tests/31_multirules.cf" (tmp </> "grammar.cf")
+        cd tmp
+        cmd "bnfc" "--java" "grammar.cf"
