710a98add1dc652cae828a4bc48b10456d8bcd5b
Pascal Hof <pascal.hof@tu-dortmund.de>
2016-02-03 12:17:07 +0100

    converted tabs to spaces

--- source/src/BNFC/Backend/Java/Utils.hs
+++ source/src/BNFC/Backend/Java/Utils.hs
@@ -3,16 +3,16 @@ import BNFC.Backend.Common.NamedVariables
 import BNFC.Utils ( mkName, NameStyle(..))
 
 javaReserved = [
-    "abstract" 	,"continue" 	,"for" 	,"new" 	,"switch"
-    ,"assert"  ,"default" 	,"goto" 	,"package" 	,"synchronized"
-    ,"boolean" 	,"do" 	,"if" 	,"private" 	,"this"
-    ,"break" 	,"double" 	,"implements" 	,"protected" 	,"throw"
-    ,"byte" 	,"else" 	,"import" 	,"public" 	,"throws"
-    ,"case" 	,"enum" 	,"instanceof" 	,"return" 	,"transient"
-    ,"catch" 	,"extends" 	,"int" 	,"short" 	,"try"
-    ,"char" 	,"final" 	,"interface" 	,"static" 	,"void"
-    ,"class" 	,"finally" 	,"long" 	,"strictfp" 	,"volatile"
-    ,"const" 	,"float" 	,"native" 	,"super" 	,"while"
+    "abstract"   ,"continue"   ,"for"   ,"new"   ,"switch"
+    ,"assert"  ,"default"   ,"goto"   ,"package"   ,"synchronized"
+    ,"boolean"   ,"do"   ,"if"   ,"private"   ,"this"
+    ,"break"   ,"double"   ,"implements"   ,"protected"   ,"throw"
+    ,"byte"   ,"else"   ,"import"   ,"public"   ,"throws"
+    ,"case"   ,"enum"   ,"instanceof"   ,"return"   ,"transient"
+    ,"catch"   ,"extends"   ,"int"   ,"short"   ,"try"
+    ,"char"   ,"final"   ,"interface"   ,"static"   ,"void"
+    ,"class"   ,"finally"   ,"long"   ,"strictfp"   ,"volatile"
+    ,"const"   ,"float"   ,"native"   ,"super"   ,"while"
     ]
 
 getRuleName z = if x `elem` ("grammar" : javaReserved) then z ++ "_" else z
@@ -21,5 +21,5 @@ getRuleName z = if x `elem` ("grammar" : javaReserved) then z ++ "_" else z
 getLabelName = mkName ["Rule"] CamelCase
 
 getLastInPackage :: String -> String
-getLastInPackage = 
+getLastInPackage =
     last . words . map (\c -> if c == '.' then ' ' else c)
