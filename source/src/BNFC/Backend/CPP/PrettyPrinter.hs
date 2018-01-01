BNFC/bnfc

commit e8107a065a2b00ee8fb362886d99416026eb14b1

Fixed cpp-nostl compilation error

source/src/BNFC/Backend/CPP/PrettyPrinter.hs
@@ -82,8 +82,8 @@ mkHFile useStl inPackage cf groups = unlines
     "  /* The following are simple heuristics for rendering terminals */",
     "  /* You may wish to change them */",
     "  void render(Char c);",
-    "  void render(String s);",
-    if useStl then "void render(const char *s);" else "",
+    if useStl then "  void render(String s);" else "",
+    "void render(const char *s);",
     "  void indent(void);",
     "  void backup(void);",
     " public:",

commit ccaaca4d9182efb67f0a06c644df51eaa4eef3aa

[ cpp ] Fixed indentation
Issue introduced by PR #195, commit e8107a065a2b00ee8fb362886d99416026eb14b1

source/src/BNFC/Backend/CPP/PrettyPrinter.hs
@@ -83,7 +83,7 @@ mkHFile useStl inPackage cf groups = unlines
     "  /* You may wish to change them */",
     "  void render(Char c);",
     if useStl then "  void render(String s);" else "",
-    "void render(const char *s);",
+    "  void render(const char *s);",
     "  void indent(void);",
     "  void backup(void);",
     " public:",