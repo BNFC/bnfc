30260b7777047c0ed73bfe888c8d5c63493e3a0a
Andreas Abel <andreas.abel@ifi.lmu.de>
2020-01-19 16:23:43 +0100

    [ #249 C ] fixed printer: use renderS instead of renderC for unicode

--- source/src/BNFC/Backend/Common/StrUtils.hs
+++ source/src/BNFC/Backend/Common/StrUtils.hs
@@ -1,5 +1,7 @@
 module BNFC.Backend.Common.StrUtils where
 
+import Data.Char (ord)
+
 -- | Function that, given an input string, renders it either as a char (if
 -- it has legth 1) or a string. It should also excape characters correctly.
 -- The first returned value is the 'type' of the string: either C for char
@@ -15,8 +17,8 @@ module BNFC.Backend.Common.StrUtils where
 -- >>> renderCharOrString "\"\\'"
 -- ('S',"\"\\\"\\\\\\'\"")
 renderCharOrString :: String -> (Char, String)
-renderCharOrString [char] = ('C', show char)     -- using show shoud quote '
-renderCharOrString s =('S', "\"" ++ escapeChars s ++ "\"")
+renderCharOrString [c] | ord c <= 255 = ('C', show c)     -- using show should quote '
+renderCharOrString s = ('S', "\"" ++ escapeChars s ++ "\"")
 
 -- | Helper function that escapes characters in strings
 -- >>> escapeChars "\\"
@@ -31,5 +33,3 @@ escapeChars ('\\':xs) = '\\' : '\\' : escapeChars xs
 escapeChars ('\"':xs) = '\\' : '\"' : escapeChars xs
 escapeChars ('\'':xs) = '\\' : '\'' : escapeChars xs
 escapeChars (x:xs) = x : escapeChars xs
-
-
