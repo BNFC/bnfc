BNFC/bnfc

commit b8701c34ff3575de3790cdefb39a212c427aed0d

[ fixed #276 ] escape tab, newline etc in ANTLR lexer

source/src/BNFC/Backend/Java/RegToAntlrLexer.hs
@@ -2,6 +2,8 @@ module BNFC.Backend.Java.RegToAntlrLexer (printRegJLex, escapeChar) where
 
 -- modified from RegToJLex.hs
 
+import Data.Char (showLitChar)
+
 import AbsBNF
 
 -- the top-level printing method
@@ -41,7 +43,7 @@ instance Print Char where
 
 escapeChar :: Char -> String
 escapeChar x | x `elem` reserved = '\\' : [x]
-escapeChar x = [x]
+escapeChar x = showLitChar x ""
 
 -- Characters that must be escaped in ANTLR regular expressions
 reserved :: [Char]

commit 3745b86a76760faebf277cc9414383eb5070367e

[ #249 Java ] fixed lexer regression introduced by #257 / #276
Don't use showLitChar for unicode characters!

b8701c34ff3575de3790cdefb39a212c427aed0d broke #249 for Java/ANTLR in the lexer,
c49d1fdc174281b95ef8feca611e3899260dbf73 for Java/CUP.

source/src/BNFC/Backend/Java/RegToAntlrLexer.hs
@@ -2,7 +2,7 @@ module BNFC.Backend.Java.RegToAntlrLexer (printRegJLex, escapeChar) where
 
 -- modified from RegToJLex.hs
 
-import Data.Char (showLitChar)
+import Data.Char (ord, showLitChar)
 
 import AbsBNF
 
@@ -42,8 +42,10 @@ instance Print Char where
   prtList = map (concat . prt 0)
 
 escapeChar :: Char -> String
-escapeChar x | x `elem` reserved = '\\' : [x]
-escapeChar x = showLitChar x ""
+escapeChar x
+  | x `elem` reserved = '\\' : [x]
+  | ord x >= 256      = [x]
+  | otherwise         = showLitChar x ""
 
 -- Characters that must be escaped in ANTLR regular expressions
 reserved :: [Char]

source/src/BNFC/Backend/Java/RegToJLex.hs
@@ -2,7 +2,7 @@ module BNFC.Backend.Java.RegToJLex (printRegJLex, escapeChar) where
 
 -- modified from pretty-printer generated by the BNF converter
 
-import Data.Char (showLitChar)
+import Data.Char (ord, showLitChar)
 import AbsBNF
 import BNFC.Options (JavaLexerParser(..))
 
@@ -44,8 +44,10 @@ instance Print Char where
 escapeChar :: JavaLexerParser -> Char -> String
 escapeChar _ '^' = "\\x5E" -- special case, since \^ is a control character escape
 escapeChar JFlexCup x | x `elem` jflexReserved = '\\' : [x]
-escapeChar _ x | x `elem` jlexReserved = '\\' : [x]
-escapeChar _ x = showLitChar x ""
+escapeChar _ x
+  | x `elem` jlexReserved = '\\' : [x]
+  | ord x >= 255          = [x]
+  | otherwise             = showLitChar x ""
 
 -- Characters that must be escaped in JLex regular expressions
 jlexReserved :: [Char]