BNFC/bnfc

commit e8abd9f99b04d3e4c2ee19f194a119f017ad2a97

Fix generated C++ printer for coercion lists (issue #100)
This commit also merges the pretty printer generator for the two
flavours of C++ (with and without STL).

source/src/BNFC/Backend/CPP/NoSTL/CFtoCPPAbs.hs
@@ -44,6 +44,7 @@ module BNFC.Backend.CPP.NoSTL.CFtoCPPAbs (cf2CPPAbs) where
 import BNFC.CF
 import BNFC.Utils((+++),(++++))
 import BNFC.Backend.Common.NamedVariables
+import BNFC.Backend.Common.OOAbstract
 import Data.List
 import Data.Char(toLower)
 import Text.PrettyPrint
@@ -72,32 +73,16 @@ mkHFile cf = unlines
   prVisitable,
   "",
   "/********************   Abstract Syntax Classes    ********************/\n",
-  concatMap (prDataH user) (cf2dataLists cf),
+  concatMap (prDataH user) (getAbstractSyntax cf),
   "",
   "#endif"
  ]
  where
   user = fst (unzip (tokenPragmas cf))
-  header = "//C++ Abstract Syntax Interface generated by the BNF Converter.\n"
-  rules = getRules cf
-  classes = rules ++ (getClasses (map show $ allCats cf))
-  prForward s | isProperLabel s = "class " ++ (show$normCat$strToCat s) ++ ";\n"
+  header = "/* ~~~ C++ Abstract Syntax Interface generated by the BNF Converter.\n ~~~ */"
+  classes = allClasses (cf2cabs cf)
+  prForward s | isProperLabel s = "class " ++ s ++ ";\n"
   prForward _ = ""
-  getRules cf = (map testRule (rulesOfCF cf))
-  getClasses [] = []
-  getClasses (c:cs) =
-   if identCat (normCat$strToCat c) /= c
-   then getClasses cs
-   else if elem c rules
-     then getClasses cs
-     else c : (getClasses cs)
-
-  testRule (Rule f c _) =
-   if isList c
-   then if isConsFun f
-     then identCat c
-     else "_" --ignore this
-   else f
 
 --Prints interface classes for all categories.
 prDataH :: [UserDef] -> Data -> String
@@ -143,22 +128,21 @@ prRuleH user c (fun, cats) =
     else --a standard rule
     unlines
     [
-     "class" +++ fun' +++ ": public" +++ super,
+     "class" +++ fun +++ ": public" +++ super,
      "{",
      " public:",
      render $ nest 2 $ prInstVars user vs,
-     "  " ++ fun' ++ "(const" +++ fun' +++ "&);",
-     "  " ++ fun' ++ " &operator=(const" +++ fun' +++ "&);",
-     "  " ++ fun' ++ "(" ++ (prConstructorH 1 vs) ++ ");",
-     prDestructorH fun',
+     "  " ++ fun ++ "(const" +++ fun +++ "&);",
+     "  " ++ fun ++ " &operator=(const" +++ fun +++ "&);",
+     "  " ++ fun ++ "(" ++ (prConstructorH 1 vs) ++ ");",
+     prDestructorH fun,
      "  virtual void accept(Visitor *v);",
-     "  virtual " +++ fun' +++ " *clone() const;",
-     "  void swap(" ++ fun' +++ "&);",
+     "  virtual " +++ fun +++ " *clone() const;",
+     "  void swap(" ++ fun +++ "&);",
      "};\n"
     ]
    where
      vs = getVars cats
-     fun' = normFun fun
      c' = identCat (normCat c);
      mem = drop 4 c'
      memstar = if isBasic user mem then "" else "*"
@@ -206,9 +190,7 @@ prVisitor fs = unlines
      "};"
     ]
    prVisitFun f | isProperLabel f =
-     "  virtual void visit" ++ f' ++ "(" ++ f' ++ " *p) = 0;\n"
-    where
-       f' = normFun f
+     "  virtual void visit" ++ f ++ "(" ++ f ++ " *p) = 0;\n"
    prVisitFun _ = ""
 
 --typedefs in the Header make generation much nicer.
@@ -257,7 +239,7 @@ mkCFile :: CF -> String
 mkCFile cf = unlines
  [
   header,
-  concatMap (prDataC user) (cf2dataLists cf)
+  concatMap (prDataC user) (getAbstractSyntax cf)
  ]
  where
   user = fst (unzip (tokenPragmas cf))
@@ -292,17 +274,16 @@ prRuleC user c (fun, cats) =
     else --a standard rule
     unlines
     [
-     "/********************   " ++ fun' ++ "    ********************/",
-     render $ prConstructorC user fun' vs cats,
-     prCopyC user fun' vs,
-     prDestructorC user fun' vs,
+     "/********************   " ++ fun ++ "    ********************/",
+     render $ prConstructorC user fun vs cats,
+     prCopyC user fun vs,
+     prDestructorC user fun vs,
      prAcceptC fun,
-     prCloneC user fun' vs,
+     prCloneC user fun vs,
      ""
     ]
    where
      vs = getVars cats
-     fun' = normFun fun
      c' = identCat (normCat c)
 
 --These are all built-in list functions.

source/src/BNFC/Backend/CPP/PrettyPrinter.hs
@@ -20,26 +20,35 @@
    **************************************************************
 -}
 
-module BNFC.Backend.CPP.NoSTL.CFtoCPPPrinter (cf2CPPPrinter) where
+module BNFC.Backend.CPP.PrettyPrinter (cf2CPPPrinter, prRender) where
 
 import BNFC.CF
 import BNFC.Utils ((+++))
+import BNFC.Backend.Common
 import BNFC.Backend.Common.NamedVariables
 import BNFC.Backend.Common.StrUtils (renderCharOrString)
+import Data.List
 import Data.Char(toLower)
-import Text.PrettyPrint
+import BNFC.Backend.CPP.STL.STLUtils
+import BNFC.PrettyPrint
 
 --Produces (.H file, .C file)
-cf2CPPPrinter :: CF -> (String, String)
-cf2CPPPrinter cf = (mkHFile cf groups, mkCFile cf groups)
+cf2CPPPrinter :: Bool -> Maybe String -> CF -> (String, String)
+cf2CPPPrinter useStl inPackage cf =
+    (mkHFile useStl inPackage cf groups, mkCFile useStl inPackage cf groups)
  where
-    groups = fixCoercions (ruleGroupsInternals cf)
+    groups = positionRules cf ++ (fixCoercions (ruleGroupsInternals cf))
+
+positionRules :: CF -> [(Cat,[Rule])]
+positionRules cf =
+      [(cat,[Rule (show cat) cat [Left catString, Left catInteger]]) |
+        cat <- filter (isPositionCat cf) $ fst (unzip (tokenPragmas cf))]
 
 {- **** Header (.H) File Methods **** -}
 
 --An extremely large function to make the Header File
-mkHFile :: CF -> [(Cat,[Rule])] -> String
-mkHFile _ groups = unlines
+mkHFile :: Bool -> Maybe String -> CF -> [(Cat,[Rule])] -> String
+mkHFile useStl inPackage cf groups = unlines
  [
   printHeader,
   concatMap prDataH groups,
@@ -52,19 +61,20 @@ mkHFile _ groups = unlines
  where
   printHeader = unlines
    [
-    "#ifndef PRINTER_HEADER",
-    "#define PRINTER_HEADER",
+    "#ifndef " ++ hdef,
+    "#define " ++ hdef,
     "",
     "#include \"Absyn.H\"",
     "#include <stdio.h>",
     "#include <string.h>",
     "#include <stdlib.h>",
     "",
+    nsStart inPackage,
     "/* Certain applications may improve performance by changing the buffer size */",
-    "#define BUFFER_INITIAL 2000",
+    "#define " ++ nsDefine inPackage "BUFFER_INITIAL" ++ " 2000",
     "/* You may wish to change _L_PAREN or _R_PAREN */",
-    "#define _L_PAREN '('",
-    "#define _R_PAREN ')'",
+    "#define " ++ nsDefine inPackage "_L_PAREN" ++ " '('",
+    "#define " ++ nsDefine inPackage "_R_PAREN" ++ " ')'",
     "",
     "class PrintAbsyn : public Visitor",
     "{",
@@ -74,6 +84,7 @@ mkHFile _ groups = unlines
     "  /* You may wish to change them */",
     "  void render(Char c);",
     "  void render(String s);",
+    if useStl then "void render(char* s);" else "",
     "  void indent(void);",
     "  void backup(void);",
     " public:",
@@ -81,13 +92,16 @@ mkHFile _ groups = unlines
     "  ~PrintAbsyn(void);",
     "  char* print(Visitable* v);"
    ]
-  classFooter = unlines
+  hdef = nsDefine inPackage "PRINTER_HEADER"
+  classFooter = unlines $
    [
     "  void visitInteger(Integer i);",
     "  void visitDouble(Double d);",
     "  void visitChar(Char c);",
     "  void visitString(String s);",
-    "  void visitIdent(String s);",
+    "  void visitIdent(String s);"
+   ] ++ ["  void visit" ++ t ++ "(String s);" | t <- tokenNames cf] ++
+   [
     " protected:",
     "  void inline bufAppend(const char* s)",
     "  {",
@@ -115,10 +129,11 @@ mkHFile _ groups = unlines
     "    cur_++;",
     "    buf_[cur_] = 0;",
     "  }",
+    if useStl then render (nest 2 bufAppendString) else "",
     "  void inline bufReset(void)",
     "  {",
     "    cur_ = 0;",
-    "    buf_size = BUFFER_INITIAL;",
+    "    buf_size = " ++ nsDefine inPackage "BUFFER_INITIAL" ++ ";",
     "    resizeBuffer();",
     "    memset(buf_, 0, buf_size);",
     "  }",
@@ -142,6 +157,12 @@ mkHFile _ groups = unlines
     "};",
     ""
    ]
+  bufAppendString :: Doc
+  bufAppendString =
+      "void inline bufAppend(String str)"
+      $$ codeblock 2
+          [ "const char* s = str.c_str();"
+          , "bufAppend(s);"]
   showHeader = unlines
    [
     "",
@@ -154,6 +175,7 @@ mkHFile _ groups = unlines
    ]
   footer = unlines
    [
+    nsEnd inPackage,
     "",
     "#endif"
    ]
@@ -179,24 +201,28 @@ prRuleH _ = ""
 {- **** Implementation (.C) File Methods **** -}
 
 --This makes the .C file by a similar method.
-mkCFile :: CF -> [(Cat,[Rule])] -> String
-mkCFile cf groups = concat
+mkCFile :: Bool -> Maybe String -> CF -> [(Cat,[Rule])] -> String
+mkCFile useStl inPackage cf groups = concat
    [
     header,
-    prRender,
+    nsStart inPackage ++ "\n",
+    prRender useStl,
     printEntries,
-    concatMap (prPrintData user) groups,
+    concatMap (prPrintData useStl inPackage cf) groups,
     printBasics,
+    printTokens,
     showEntries,
-    concatMap prShowData groups,
-    showBasics
+    concatMap (prShowData useStl) groups,
+    showBasics,
+    showTokens,
+    nsEnd inPackage ++ "\n"
    ]
   where
-    user = fst (unzip (tokenPragmas cf))
     header = unlines
      [
       "/*** BNFC-Generated Pretty Printer and Abstract Syntax Viewer ***/",
       "",
+      "#include <string>",
       "#include \"Printer.H\"",
       "#define INDENT_WIDTH 2",
       ""
@@ -273,6 +299,17 @@ mkCFile cf groups = concat
       "}",
       ""
      ]
+
+    printTokens = unlines
+     [unlines [
+      "void PrintAbsyn::visit" ++ t ++ "(String s)",
+      "{",
+      "  render(s);",
+      "}",
+      ""
+      ] | t <- tokenNames cf
+     ]
+
     showBasics = unlines
      [
       "void ShowAbsyn::visitInteger(Integer i)",
@@ -308,33 +345,116 @@ mkCFile cf groups = concat
       ""
      ]
 
+    showTokens = unlines
+     [unlines [
+      "void ShowAbsyn::visit" ++ t ++ "(String s)",
+      "{",
+      "  bufAppend('\\\"');",
+      "  bufAppend(s);",
+      "  bufAppend('\\\"');",
+      "}",
+      ""
+      ] | t <- tokenNames cf
+     ]
+
+
 {- **** Pretty Printer Methods **** -}
 
 --Generates methods for the Pretty Printer
-prPrintData :: [UserDef] -> (Cat, [Rule]) -> String
-prPrintData _ (cat@(ListCat c), rules) =
- unlines
- [
-  "void PrintAbsyn::visit" ++ cl ++ "("++ cl ++ " *" ++ vname ++ ")",
-  "{",
-  "  while(" ++ vname ++ "!= 0)",
-  "  {",
-  "    if (" ++ vname ++ "->" ++ vname ++ "_ == 0)",
-  "    {",
-  visitMember,
-  optsep,
-  "      " ++ vname +++ "= 0;",
-  "    }",
-  "    else",
-  "    {",
-  visitMember,
-  "      render(" ++ sep ++ ");",
-  "      " ++ vname +++ "=" +++ vname ++ "->" ++ vname ++ "_;",
-  "    }",
-  "  }",
-  "}",
-  ""
- ]
+prPrintData :: Bool -> Maybe String -> CF -> (Cat, [Rule]) -> String
+prPrintData True {- use STL -} _ _ (cat@(ListCat _), rules) =
+    render $ genPrintVisitorList (cat, rules)
+prPrintData False {- use STL -} _ _ (cat@(ListCat _), rules) =
+    genPrintVisitorListNoStl (cat, rules)
+prPrintData _ inPackage cf (cat, rules) = -- Not a list
+ -- a position token
+ if isPositionCat cf cat then unlines [
+   "void PrintAbsyn::visit" ++ show cat ++ "(" ++ show cat ++ "* p)",
+   "{",
+   "  visitIdent(p->string_);",
+   "}"
+   ]
+ else abstract ++ (concatMap (prPrintRule inPackage) rules)
+ where
+   cl = identCat (normCat cat)
+   abstract = case lookupRule (show cat) rules of
+    Just _ -> ""
+    Nothing ->  "void PrintAbsyn::visit" ++ cl ++ "(" ++ cl ++ "*p) {} //abstract class\n\n"
+
+-- | Generate pretty printer visitor for a list category:
+--
+-- >>> let c = Cat "C" ; lc = ListCat c
+-- >>> let rules = [Rule "[]" lc [], Rule "(:)" lc [Left c, Right "-", Left lc]]
+-- >>> genPrintVisitorList (lc, rules)
+-- void PrintAbsyn::visitListC(ListC *listc)
+-- {
+--   for (ListC::const_iterator i = listc->begin() ; i != listc->end() ; ++i)
+--   {
+--     (*i)->accept(this);
+--     render('-');
+--   }
+-- }
+--
+-- >>> let c2 = CoercCat "C" 2 ; lc2 = ListCat c2
+-- >>> let rules2 = rules ++ [Rule "[]" lc2 [], Rule "(:)" lc2 [Left c2, Right "+", Left lc2]]
+-- >>> genPrintVisitorList (lc, rules2)
+-- void PrintAbsyn::visitListC(ListC *listc)
+-- {
+--   for (ListC::const_iterator i = listc->begin() ; i != listc->end() ; ++i)
+--   {
+--     (*i)->accept(this);
+--     switch(_i_)
+--     {
+--       case 2: render('+'); break;
+--       default: render('-');
+--     }
+--   }
+-- }
+genPrintVisitorList :: (Cat, [Rule]) -> Doc
+genPrintVisitorList (cat@(ListCat c), rules) =
+    "void PrintAbsyn::visit" <> text cl <> "("<> text cl <> " *" <> vname <> ")"
+    $$ codeblock 2
+      [ "for ("<> text cl <> "::const_iterator i = " <> vname <> "->begin() ; i != " <> vname <> "->end() ; ++i)"
+      , codeblock 2
+          [ if isTokenCat c
+              then "visit" <> text (baseName cl) <> "(*i) ;"
+              else "(*i)->accept(this);"
+          , (if hasOneFunc rules
+              then "if (i != " <> vname <> "->end() - 1)"
+              else empty)
+            <+> renderListSepByPrecedence "_i_" renderSep separators
+          ]
+      ]
+ where
+   separators = getSeparatorByPrecedence rules
+   cl = identCat (normCat cat)
+   vname = text $ map toLower cl
+   renderSep s = "render(" <> text (snd (renderCharOrString s)) <> ")"
+
+-- | This is the only part of the pretty printer that differs significantly
+-- between the versions with and without STL.
+genPrintVisitorListNoStl :: (Cat, [Rule]) -> String
+genPrintVisitorListNoStl (cat@(ListCat c), rules) = unlines
+    [ "void PrintAbsyn::visit" ++ cl ++ "("++ cl ++ " *" ++ vname ++ ")"
+    , "{"
+    , "  while(" ++ vname ++ "!= 0)"
+    , "  {"
+    , "    if (" ++ vname ++ "->" ++ vname ++ "_ == 0)"
+    , "    {"
+    , visitMember
+    , optsep
+    , "      " ++ vname +++ "= 0;"
+    , "    }"
+    , "    else"
+    , "    {"
+    , visitMember
+    , render $ nest 6 $ renderListSepByPrecedence "_i_" renderSep separators
+    , "      " ++ vname +++ "=" +++ vname ++ "->" ++ vname ++ "_;"
+    , "    }"
+    , "  }"
+    , "}"
+    , ""
+    ]
   where
     visitMember = if isTokenCat c
         then "      visit" ++ funName c ++ "(" ++ vname ++ "->" ++ member ++ ");"
@@ -346,78 +466,67 @@ prPrintData _ (cat@(ListCat c), rules) =
     optsep = if hasOneFunc rules then "" else ("      render(" ++ sep ++ ");")
     sep = snd (renderCharOrString sep')
     sep' = getCons rules
-prPrintData user (cat, rules) = --Not a list:
-    abstract ++ concatMap (render . prPrintRule user) rules
-  where
-    cl = identCat (normCat cat)
-    abstract = case lookupRule (show cat) rules of
-      Just _ -> ""
-      Nothing ->  "void PrintAbsyn::visit" ++ cl ++ "(" ++ cl ++ "*p) {} //abstract class\n\n"
+    renderSep s = "render(" <> text (snd (renderCharOrString s)) <> ")"
+    separators = getSeparatorByPrecedence rules
 
--- | Pretty Printer methods for a rule.
--- >>> prPrintRule [Cat "A"] (Rule "Myf" (Cat "X") [Left (TokenCat "A"), Left (TokenCat "Integer"), Right "abc", Left (Cat "B")])
--- void PrintAbsyn::visitMyf(Myf* p)
--- { int oldi = _i_;
---   if (oldi > 0) render(_L_PAREN);
---   visitIdent(p->a_);
---   visitInteger(p->integer_);
---   render("abc");
---   _i_ = 0; p->b_->accept(this);
---   if (oldi > 0) render(_R_PAREN);
---   _i_ = oldi;
--- }
-prPrintRule :: [UserDef] -> Rule -> Doc
-prPrintRule _ r@(Rule fun _ cats) | isProperLabel fun = vcat
-    [ "void PrintAbsyn::visit" <> text fun <> parens (text fun <> "*" <+>  fnm)
-    , "{"
-    , nest 2 (vcat
-        [ "int oldi = _i_;"
-        , "if (oldi > " <> integer p <> ") render(_L_PAREN);"
-        , vcat cats'
-        , "if" <+> parens ("oldi >" <+> integer p) <+> "render(_R_PAREN);"
-        , "_i_ = oldi;" ])
-    , "}"
-    ]
+--Pretty Printer methods for a rule.
+prPrintRule :: Maybe String -> Rule -> String
+prPrintRule inPackage r@(Rule fun _ cats) | isProperLabel fun = unlines
+  [
+   "void PrintAbsyn::visit" ++ fun ++ "(" ++ fun ++ "*" +++ fnm ++ ")",
+   "{",
+   "  int oldi = _i_;",
+   lparen,
+   cats',
+   rparen,
+   "  _i_ = oldi;",
+   "}\n"
+  ]
    where
     p = precRule r
-    cats' = map (prPrintCat fnm) (numVars cats)
+    (lparen, rparen) =
+      ("  if (oldi > " ++ show p ++ ") render(" ++ nsDefine inPackage "_L_PAREN" ++ ");\n",
+       "  if (oldi > " ++ show p ++ ") render(" ++ nsDefine inPackage "_R_PAREN" ++ ");\n")
+    cats' = concatMap (prPrintCat fnm) (numVars cats)
     fnm = "p" --old names could cause conflicts
 prPrintRule _ _ = ""
 
--- | This goes on to recurse to the instance variables.
--- >>> prPrintCat "MyFun" (Left (Cat "A", "a_"))
--- _i_ = 0; MyFun->a_->accept(this);
--- >>> prPrintCat "MyFun" (Left (CoercCat "Exp" 2, "exp_"))
--- _i_ = 2; MyFun->exp_->accept(this);
--- >>> prPrintCat "MyFun" (Right "abc")
--- render("abc");
--- >>> prPrintCat "MyFun" (Left (TokenCat "A", "a_"))
--- visitIdent(MyFun->a_);
--- >>> prPrintCat "MyFun" (Left (TokenCat "Integer", "integer_"))
--- visitInteger(MyFun->integer_);
-prPrintCat :: Doc -> Either (Cat, Doc) String -> Doc
-prPrintCat _ (Right t) =
-    "render" <> parens (text (snd (renderCharOrString t))) <> ";"
-prPrintCat fnm (Left (cat, nt)) | isTokenCat cat =
-    "visit" <> text (funName cat) <> "(" <> fnm <> "->" <> nt <> ");"
-prPrintCat fnm (Left (cat, nt)) =
-    if isList cat
-    then "if(" <> fnm <> "->" <> nt <> ")" <+> braces accept
-    else accept
+--This goes on to recurse to the instance variables.
+prPrintCat :: String -> Either (Cat, Doc) String -> String
+prPrintCat _ (Right t) = "  render(" ++ t' ++ ");\n"
+  where t' = snd (renderCharOrString t)
+prPrintCat fnm (Left (c, nt))
+  | isTokenCat c  = "  visit" ++ funName c ++ "(" ++ fnm ++ "->" ++ render nt ++ ");\n"
+  | isList c            = "  if(" ++ fnm ++ "->" ++ render nt ++ ") {" ++ accept ++ "}"
+  | otherwise           = "  " ++ accept ++ "\n"
   where
-      accept = if cat == InternalCat
-               then "/* Internal Category */"
-               else setI (precCat cat) <+> fnm <> "->" <> nt <> "->accept(this);"
-      -- Just sets the coercion level for parentheses in the Pretty Printer.
-      setI :: Integer -> Doc
-      setI n = "_i_ =" <+> integer n <> ";"
-
+    accept
+      | c == InternalCat =  "/* Internal Category */\n"
+      | otherwise        = setI (precCat c) ++ fnm ++ "->" ++ render nt ++ "->accept(this);"
 
 {- **** Abstract Syntax Tree Printer **** -}
 
 --This prints the functions for Abstract Syntax tree printing.
-prShowData :: (Cat, [Rule]) -> String
-prShowData (cat@(ListCat c), _) =
+prShowData :: Bool -> (Cat, [Rule]) -> String
+prShowData True (cat@(ListCat c), _) = unlines
+ [
+  "void ShowAbsyn::visit" ++ cl ++ "("++ cl ++ " *" ++ vname ++ ")",
+  "{",
+  "  for ("++ cl ++"::const_iterator i = " ++
+       vname++"->begin() ; i != " ++vname ++"->end() ; ++i)",
+  "  {",
+  if isTokenCat c
+    then "    visit" ++ baseName cl ++ "(*i) ;"
+    else "    (*i)->accept(this);",
+  "    if (i != " ++ vname ++ "->end() - 1) bufAppend(\", \");",
+  "  }",
+  "}",
+  ""
+ ]
+  where
+    cl = identCat (normCat cat)
+    vname = map toLower cl
+prShowData False (cat@(ListCat c), _) =
  unlines
  [
   "void ShowAbsyn::visit" ++ cl ++ "("++ cl ++ " *" ++ vname ++ ")",
@@ -447,8 +556,8 @@ prShowData (cat@(ListCat c), _) =
     visitMember = if isTokenCat c
       then "      visit" ++ funName c ++ "(" ++ vname ++ "->" ++ member ++ ");"
       else "      " ++ vname ++ "->" ++ member ++ "->accept(this);"
-prShowData (cat, rules) = --Not a list:
-    abstract ++ concatMap prShowRule rules
+prShowData _ (cat, rules) =  --Not a list:
+  abstract ++ (concatMap prShowRule rules)
   where
     cl = identCat (normCat cat)
     abstract = case lookupRule (show cat) rules of
@@ -479,7 +588,7 @@ prShowRule (Rule fun _ cats) | isProperLabel fun = concat
     insertSpaces (x:[]) = [x]
     insertSpaces (x:xs) = if x == ""
       then insertSpaces xs
-      else (x : ["  bufAppend(' ');\n"]) ++ insertSpaces xs
+      else (x : ["  bufAppend(' ');\n"]) ++ (insertSpaces xs)
     allTerms [] = True
     allTerms (Left _:_) = False
     allTerms (_:zs) = allTerms zs
@@ -488,37 +597,48 @@ prShowRule _ = ""
 
 --This recurses to the instance variables of a class.
 prShowCat :: String -> Either (Cat, Doc) String -> String
-prShowCat fnm c =
-  case c of
-    Right _ -> ""
-    Left (cat, nt) ->
-      if isTokenCat cat
-       then "  visit" ++ funName cat ++ "(" ++ fnm ++ "->" ++ render nt ++ ");\n"
-       else if cat == InternalCat
-       then "/* Internal Category */\n"
-       else if ((show$normCat$strToCat$render nt) /= render nt)
-          then accept
- 	  else concat
-	  [
+prShowCat _ (Right _)               = ""
+prShowCat fnm (Left (cat,nt))
+  | isTokenCat cat              =
+    "  visit" ++ funName cat ++ "(" ++ fnm ++ "->" ++ render nt ++ ");\n"
+  | cat == InternalCat                = "/* Internal Category */\n"
+  | (show $normCat$strToCat$render nt) /= render nt = accept
+  | otherwise                         =
+    concat [
 	   "  bufAppend('[');\n",
 	   "  if (" ++ fnm ++ "->" ++ render nt ++ ")" ++ accept,
 	   "  bufAppend(']');\n"
 	  ]
-       where
-         accept = "  " ++ fnm ++ "->" ++ render nt ++ "->accept(this);\n"
+  where accept = "  " ++ fnm ++ "->" ++ render nt ++ "->accept(this);\n"
 
 {- **** Helper Functions Section **** -}
+
+-- from ListIdent to Ident
+baseName cl = drop 4 cl
+
 
 --The visit-function name of a basic type
 funName :: Cat -> String
 funName (TokenCat c) | c `elem` builtin = c
   where builtin = ["Integer", "Char", "String", "Double", "Ident" ]
 funName _ = "Ident" --User-defined type
+--The visit-function name of a basic type
+-- funName :: String -> String
+-- funName v =
+--     if "integer_" `isPrefixOf` v then "Integer"
+--     else if "char_" `isPrefixOf` v then "Char"
+--     else if "string_" `isPrefixOf` v then "String"
+--     else if "double_" `isPrefixOf` v then "Double"
+--     else if "ident_" `isPrefixOf` v then "Ident"
+--     else "Ident" --User-defined type
 
+--Just sets the coercion level for parentheses in the Pretty Printer.
+setI :: Integer -> String
+setI n = "_i_ = " ++ (show n) ++ "; "
 
 --An extremely simple renderer for terminals.
-prRender :: String
-prRender = unlines
+prRender :: Bool -> String
+prRender useStl = unlines
   [
       "//You may wish to change render",
       "void PrintAbsyn::render(Char c)",
@@ -538,13 +658,12 @@ prRender = unlines
       "  {",
       "     backup();",
       "     bufAppend(c);",
-      "     bufAppend(' ');",
       "  }",
       "  else if (c == '}')",
       "  {",
       "     int t;",
       "     _n_ = _n_ - INDENT_WIDTH;",
-      "     for(t=0; t<INDENT_WIDTH; t++) {",
+      "     for (t=0; t<INDENT_WIDTH; t++) {",
       "       backup();",
       "     }",
       "     bufAppend(c);",
@@ -567,11 +686,19 @@ prRender = unlines
       "  else if (c == 0) return;",
       "  else",
       "  {",
+      "     bufAppend(' ');",
       "     bufAppend(c);",
       "     bufAppend(' ');",
       "  }",
       "}",
-      "void PrintAbsyn::render(String s)",
+      let renderString = "void PrintAbsyn::render(String s_)" $$ codeblock 2
+                             [ "const char *s = s_.c_str() ;"
+                             , "if(strlen(s) > 0)"
+                             , codeblock 2
+                                 [ "bufAppend(s);"
+                                 , "bufAppend(' ');" ] ]
+      in if useStl then render renderString else "",
+      "void PrintAbsyn::render(char* s)",
       "{",
       "  if(strlen(s) > 0)",
       "  {",

source/BNFC.cabal
@@ -136,10 +136,10 @@ Executable bnfc
 
     -- C++ backend
     BNFC.Backend.CPP.NoSTL,
+    BNFC.Backend.CPP.PrettyPrinter,
     BNFC.Backend.CPP.NoSTL.RegToFlex,
     BNFC.Backend.CPP.NoSTL.CFtoFlex,
     BNFC.Backend.CPP.NoSTL.CFtoBison,
-    BNFC.Backend.CPP.NoSTL.CFtoCPPPrinter,
     BNFC.Backend.CPP.NoSTL.CFtoCPPAbs,
 
     -- C++ STL backend
@@ -148,7 +148,6 @@ Executable bnfc
     BNFC.Backend.CPP.STL.CFtoSTLAbs,
     BNFC.Backend.CPP.STL.STLUtils,
     BNFC.Backend.CPP.STL.CFtoCVisitSkelSTL,
-    BNFC.Backend.CPP.STL.CFtoSTLPrinter,
 
     -- C# backend
     BNFC.Backend.CSharp,

source/src/BNFC/Backend/CPP/NoSTL.hs
@@ -27,7 +27,7 @@ import BNFC.Backend.CPP.NoSTL.CFtoCPPAbs
 import BNFC.Backend.CPP.NoSTL.CFtoFlex
 import BNFC.Backend.CPP.NoSTL.CFtoBison
 import BNFC.Backend.CPP.NoSTL.CFtoCVisitSkel
-import BNFC.Backend.CPP.NoSTL.CFtoCPPPrinter
+import BNFC.Backend.CPP.PrettyPrinter
 import Data.Char
 import qualified BNFC.Backend.Common.Makefile as Makefile
 
@@ -45,7 +45,7 @@ makeCppNoStl opts cf = do
     let (skelH, skelC) = cf2CVisitSkel cf
     mkfile "Skeleton.H" skelH
     mkfile "Skeleton.C" skelC
-    let (prinH, prinC) = cf2CPPPrinter cf
+    let (prinH, prinC) = cf2CPPPrinter False Nothing cf
     mkfile "Printer.H" prinH
     mkfile "Printer.C" prinC
     mkfile "Test.C" (cpptest cf)

source/src/BNFC/Backend/CPP/NoSTL/CFtoBison.hs
@@ -63,11 +63,13 @@ module BNFC.Backend.CPP.NoSTL.CFtoBison (cf2Bison) where
 import BNFC.CF
 import Data.List (intersperse)
 import BNFC.Backend.Common.NamedVariables hiding (varName)
+import BNFC.Backend.CPP.STL.CFtoBisonSTL (union)
 import Data.Char (toLower,isUpper)
 import BNFC.Utils ((+++))
 import BNFC.TypeChecker
 import ErrM
 import BNFC.Backend.C.CFtoBisonC (startSymbol)
+import BNFC.PrettyPrint
 
 --This follows the basic structure of CFtoHappy.
 
@@ -82,7 +84,7 @@ cf2Bison :: String -> CF -> SymEnv -> String
 cf2Bison name cf env
  = unlines
     [header name cf,
-     union (allCats cf),
+     render $ union Nothing (allCats cf),
      "%token _ERROR_",
      tokens user env,
      declarations cf,
@@ -120,7 +122,7 @@ header name cf = unlines
 	  "",
 	  definedRules cf,
 	  unlines $ map (parseMethod name) (allCatsNorm cf),  -- (allEntryPoints cf), M.F. 2004-09-14 fix of [Ty2] bug.
-	  concatMap reverseList (filter isList (allCats cf)),
+	  concatMap reverseList (filter isList (allCatsNorm cf)),
 	  "%}"
 	  ]
 
@@ -220,27 +222,6 @@ reverseList c = unlines
   c' = identCat (normCat c)
   v = (map toLower c') ++ "_"
 
---The union declaration is special to Bison/Yacc and gives the type of yylval.
---For efficiency, we may want to only include used categories here.
-union :: [Cat] -> String
-union cats = unlines
- [
-  "%union",
-  "{",
-  "  int int_;",
-  "  char char_;",
-  "  double double_;",
-  "  char* string_;",
-  concatMap mkPointer cats,
-  "}"
- ]
- where --This is a little weird because people can make [Exp2] etc.
- mkPointer s | identCat s /= show s = --list. add it even if it refers to a coercion.
-   "  " ++ identCat (normCat s) ++ "*" +++ varName s ++ ";\n"
- mkPointer s | normCat s == s = --normal cat
-   "  " ++ identCat (normCat s) ++ "*" +++ varName s ++ ";\n"
- mkPointer _ = ""
-
 --declares non-terminal types.
 declarations :: CF -> String
 declarations cf = concatMap (typeNT cf) (allCats cf)
@@ -283,8 +264,8 @@ constructRule cf env rules nt = (nt,[(p,(generateAction (ruleName r) b m) +++ re
      let (p,m) = generatePatterns cf env r])
  where
    ruleName r = case funRule r of
-     "(:)" -> identCat nt
-     "(:[])" -> identCat nt
+     "(:)" -> identCat (normCat nt)
+     "(:[])" -> identCat (normCat nt)
      z -> z
    revs = reversibleCats cf
    eps = allEntryPoints cf
@@ -300,7 +281,7 @@ generateAction f b ms =
   then "0;"
   else if isDefinedRule f
   then concat [ f, "_", "(", concat $ intersperse ", " ms', ");" ]
-  else concat ["new ", normFun f, "(", (concat (intersperse ", " ms')), ");"]
+  else concat ["new ", f, "(", (concat (intersperse ", " ms')), ");"]
  where
   ms' = if b then reverse ms else ms
 

source/src/BNFC/Backend/CPP/STL.hs
@@ -29,7 +29,7 @@ import BNFC.Backend.CPP.STL.CFtoSTLAbs
 import BNFC.Backend.CPP.NoSTL.CFtoFlex
 import BNFC.Backend.CPP.STL.CFtoBisonSTL
 import BNFC.Backend.CPP.STL.CFtoCVisitSkelSTL
-import BNFC.Backend.CPP.STL.CFtoSTLPrinter
+import BNFC.Backend.CPP.PrettyPrinter
 import BNFC.Backend.CPP.STL.STLUtils
 import Data.Char
 import qualified BNFC.Backend.Common.Makefile as Makefile
@@ -48,7 +48,7 @@ makeCppStl opts cf = do
     let (skelH, skelC) = cf2CVisitSkel (inPackage opts) cf
     mkfile "Skeleton.H" skelH
     mkfile "Skeleton.C" skelC
-    let (prinH, prinC) = cf2CPPPrinter (inPackage opts) cf
+    let (prinH, prinC) = cf2CPPPrinter True (inPackage opts) cf
     mkfile "Printer.H" prinH
     mkfile "Printer.C" prinC
     mkfile "Test.C" (cpptest (inPackage opts) cf)

commit 257a29c5de977f5d123c2f976e17748ddc5c4e99

[ #297 cpp-nostl ] Printer: fixed regression wrt. position tokens
Regression was introduced in e8abd9f99b04d3e4c2ee19f194a119f017ad2a97,
fix of #100, which unified the printer generators for cpp and
cpp-nostl.  Unfortunately, this did not respect the different
representation of position tokens in cpp (proper classes) and
cpp-nostl (just like tokens).

Fixed by workarounds that check useSTL.

source/src/BNFC/Backend/CPP/NoSTL/CFtoCPPAbs.hs
@@ -80,7 +80,7 @@ mkHFile cf = unlines
   "#endif"
  ]
  where
-  user = fst (unzip (tokenPragmas cf))
+  user = fst (unzip (tokenPragmas cf)) -- includes position tokens
   -- user = [ name | TokenReg name False _ <- cfgPragmas cf ]  -- position tokens are in allClasses already
   header = "/* ~~~ C++ Abstract Syntax Interface generated by the BNF Converter.\n ~~~ */"
   ca = cf2cabs cf

source/src/BNFC/Backend/CPP/PrettyPrinter.hs
@@ -41,7 +41,8 @@ cf2CPPPrinter :: Bool -> Maybe String -> CF -> (String, String)
 cf2CPPPrinter useStl inPackage cf =
     (mkHFile useStl inPackage cf groups, mkCFile useStl inPackage cf groups)
  where
-    groups = positionRules cf ++ fixCoercions (ruleGroupsInternals cf)
+    groups = when useStl (positionRules cf)  -- CPP/NoSTL treats position tokens as just tokens
+          ++ fixCoercions (ruleGroupsInternals cf)
 
 positionRules :: CF -> [(Cat,[Rule])]
 positionRules cf =