BNFC/bnfc

commit 799ab5fb80d3b40b860ceab1f058916afefa845a

[ fixed #202 ] arbitrary block comments also in Haskell
Using the functionality from generic Lexing.

source/src/BNFC/Backend/Haskell/CFtoAlex3.hs
@@ -26,7 +26,10 @@ import Data.List (intercalate, intersperse)
 
 import AbsBNF
 import BNFC.CF
+import BNFC.Lexing  (mkRegMultilineComment)
 import BNFC.Options (TokenText(..))
+import BNFC.Utils   (unless)
+
 import BNFC.Backend.Haskell.Utils
 
 cf2alex3 :: String -> String -> String -> Bool -> TokenText -> CF -> String
@@ -87,6 +90,7 @@ rMacros cf = if null symbs then [] else
 restOfAlex :: String -> Bool -> TokenText -> CF -> [String]
 restOfAlex _ shareStrings tokenText cf = [
   ":-",
+  "",
   lexComments (comments cf),
   "$white+ ;",
   pTSpec (cfgSymbols cf),
@@ -258,21 +262,29 @@ restOfAlex _ shareStrings tokenText cf = [
 
    ifC :: TokenCat -> String -> String
    ifC cat s = if isUsedCat cf (TokenCat cat) then s else ""
-   lexComments ([],[])           = []
-   lexComments (xs,s1:ys) = '\"' : s1 ++ "\"" ++ " [.]* ; -- Toss single line comments\n" ++ lexComments (xs, ys)
-   lexComments (([l1,l2],[r1,r2]):xs,[]) = concat
-                                        [
-                                        '\"':l1:l2:"\" ([$u # \\", -- FIXME quotes or escape?
-                                        r1:"] | \\",
-                                        r1:"+ [$u # [\\",
-                                        r1:" \\",
-                                        r2:"]])* (\"",
-                                        r1:"\")+ \"",
-                                        r2:"\" ;\n",
-                                        lexComments (xs, [])
-                                        ]
-   lexComments (_ : xs, []) = lexComments (xs,[])
----   lexComments (xs,(_:ys)) = lexComments (xs,ys)
+
+   lexComments
+     :: ( [(String, String)]  -- block comment delimiters
+        , [String]            -- line  comment initiators
+        ) -> String           -- Alex declarations
+   lexComments (block, line) = unlines $ concat $
+     [ unless (null line) [ "-- Line comments" ]
+     , map lexLineComment line
+     , unless (null line || null block) [ "" ]
+     , unless (null block) [ "-- Block comments" ]
+     , map (uncurry lexBlockComment) block
+     ]
+
+   lexLineComment
+     :: String   -- ^ Line comment start.
+     -> String   -- ^ Alex declaration.
+   lexLineComment s = concat [ "\"", s, "\" [.]* ;" ]
+
+   lexBlockComment
+     :: String   -- ^ Start of block comment.
+     -> String   -- ^ End of block comment.
+     -> String   -- ^ Alex declaration.
+   lexBlockComment start end = printRegAlex (mkRegMultilineComment start end) ++ " ;"
 
    -- tokens consisting of special symbols
    pTSpec [] = ""
@@ -350,7 +362,7 @@ instance Print Char where
     '\r'             -> ["\\r"]
     '\f'             -> ["\\f"]
     c | isAlphaNum c -> [[c]]
-    c | isPrint c    -> ['\\':[c]]
+    c | isPrint c    -> ['\\':[c]]  -- ['\'':c:'\'':[]] -- Does not work for )
     c                -> ['\\':show (ord c)]
 
   prtList = map (concat . prt 0)
@@ -363,18 +375,19 @@ instance Print Ident where
 
 instance Print Reg where
   prt i e = case e of
-   RSeq reg0 reg    -> prPrec i 2 (prt 2 reg0 ++ prt 3 reg)
-   RAlt reg0 reg    -> prPrec i 1 (concat [prt 1 reg0 , ["|"] , prt 2 reg])
-   RMinus reg0 reg  -> prPrec i 1 (concat [prt 2 reg0 , ["#"] , prt 2 reg])
-   RStar reg        -> prPrec i 3 (prt 3 reg ++ ["*"])
-   RPlus reg        -> prPrec i 3 (prt 3 reg ++ ["+"])
-   ROpt reg         -> prPrec i 3 (prt 3 reg ++ ["?"])
-   REps             -> prPrec i 3 ["()"]
-   RChar c          -> prPrec i 3 (prt 0 c)
-   RAlts str        -> prPrec i 3 (concat [["["],prt 0 str,["]"]])
-   RSeqs str        -> prPrec i 2 (concatMap (prt 0) str)
-   RDigit           -> prPrec i 3 ["$d"]
-   RLetter          -> prPrec i 3 ["$l"]
-   RUpper           -> prPrec i 3 ["$c"]
-   RLower           -> prPrec i 3 ["$s"]
-   RAny             -> prPrec i 3 ["$u"]
+   RSeq reg0 reg    -> prPrec i 2 $ prt 2 reg0 ++ prt 3 reg
+   RAlt reg0 reg    -> prPrec i 1 $ concat [prt 1 reg0 , ["|"] , prt 2 reg]
+   RStar reg        -> prPrec i 3 $ prt 3 reg ++ ["*"]
+   RPlus reg        -> prPrec i 3 $ prt 3 reg ++ ["+"]
+   ROpt reg         -> prPrec i 3 $ prt 3 reg ++ ["?"]
+   -- Atomic/parenthesized expressions
+   RMinus reg0 reg  -> concat [ ["["], prt 2 reg0 , ["#"] , prt 2 reg, ["]"] ]
+   REps             -> ["()"]
+   RChar c          -> prt 0 c
+   RAlts str        -> concat [["["],prt 0 str,["]"]]
+   RSeqs str        -> [show str]
+   RDigit           -> ["$d"]
+   RLetter          -> ["$l"]
+   RUpper           -> ["$c"]
+   RLower           -> ["$s"]
+   RAny             -> ["$u"]

source/src/BNFC/Lexing.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module BNFC.Lexing
-    ( mkLexer, LexType(..) ) where
+    ( mkLexer, LexType(..), mkRegMultilineComment ) where
 
 import Prelude'
 
@@ -62,7 +62,7 @@ regInteger = RPlus RDigit
 -- '"'(char-["\"\\"]|'\\'["\"\\nt"])*'"'
 regString :: Reg
 regString = RChar '"'
-            <> RStar ( RMinus RAny (RAlts "\"\\")
+            <> RStar ((RAny `RMinus` RAlts "\"\\")
                        <|> (RChar '\\' <> RAlts "\"\\nt"))
             <> RChar '"'
 
@@ -83,15 +83,15 @@ regDouble = RPlus RDigit <> RChar '.' <> RPlus RDigit
 
 -- | Create regex for single line comments
 -- >>> p $ mkRegSingleLineComment "--"
--- {"--"}(char*'\n')
+-- {"--"}char*'\n'
 mkRegSingleLineComment :: String -> Reg
-mkRegSingleLineComment s = RSeq (RSeqs s) (RSeq (RStar RAny) (RChar '\n'))
+mkRegSingleLineComment s = RSeqs s <> RStar RAny <> RChar '\n'
 
 -- | Create regex for multiline comments
 -- >>> p $ mkRegMultilineComment "<" ">"
--- '<'((char|'\n')-'>')*'>'
+-- '<'(char-'>'|'\n')*'>'
 -- >>> p $ mkRegMultilineComment "<!--" "-->"
--- {"<!--"}((char|'\n')-'-'|'-'((char|'\n')-'-')|{"--"}((char|'\n')-'>'))*'-'*{"-->"}
+-- {"<!--"}(char-'-'|'\n'|'-'(char-'-'|'\n')|{"--"}(char-'>'|'\n'))*'-'*{"-->"}
 mkRegMultilineComment :: String -> String -> Reg
 mkRegMultilineComment b e =
     foldl1 RSeq $ concat
@@ -107,6 +107,6 @@ mkRegMultilineComment b e =
     lit s = [RSeqs s]
     prefixes = map (init &&& last) (drop 1 (inits e))
     subregex =
-      [ foldr RSeq (RMinus (RAlt RAny (RChar '\n')) (RChar s)) $ lit ss
+      [ foldr RSeq ((RAny `RMinus` RChar s) `RAlt` RChar '\n') $ lit ss
       | (ss,s) <- prefixes
       ]

testing/regression-tests/202_comments/good01.in
@@ -1,0 +1,16 @@
+--( that is a comment, so main does not count here --)
+begin this comment and go until its end
+
+###
+ Let's do several lines
+ Yep!
+ end main
+ ### 
+
+[ and another ignored block comment
+  begin
+  ]
+
+main
+
+% EOF

testing/regression-tests/202_comments/good01.out
@@ -1,0 +1,10 @@
+
+Parse Successful!
+
+[Abstract Syntax]
+
+Prg
+
+[Linearized tree]
+
+main

testing/regression-tests/202_comments/test.cf
@@ -1,0 +1,7 @@
+Prg. Program ::= "main";
+
+comment "--(" "--)";
+comment "begin" "end";
+comment "###" "###";
+comment "[" "]";
+comment "%";

testing/src/ParameterizedTests.hs
@@ -61,7 +61,8 @@ current :: Test
 current = makeTestSuite "Current parameterized test" $
   map (`makeTestCase` ("regression-tests" </> cur)) parameters
   where
-  cur = "204_InternalToken"
+  cur = "202_comments"
+  -- cur = "204_InternalToken"
   -- cur = "222_IntegerList"
   -- cur = "194_layout"
   -- cur = "210_NumberedCatWithoutCoerce"
@@ -148,7 +149,8 @@ testCases :: TestParameters -> [Test]
 testCases params =
     map (makeTestCase params) $
       map ("regression-tests/" ++) $
-        [ "222_IntegerList"
+        [ "202_comments"
+        , "222_IntegerList"
         , "210_NumberedCatWithoutCoerce"
         , "204_InternalToken"
         , "249_unicode"

commit 7032f9693bd4c7898c6d1a446b367d01d7416232

[ Haskell ] lexer: fixed regression in RegExp printer
The conjunction symbol /\ has string representation "/\\" in Haskell,
but Alex expects "/\", or \/ \\ .  Reverting to the old way of printing
RSeqs fixes this regression.

This regression was introduced in
799ab5fb80d3b40b860ceab1f058916afefa845a .

source/src/BNFC/Backend/Haskell/CFtoAlex3.hs
@@ -409,7 +409,7 @@ instance Print Reg where
    REps             -> ["()"]
    RChar c          -> prt 0 c
    RAlts str        -> concat [["["],prt 0 str,["]"]]
-   RSeqs str        -> [show str]
+   RSeqs str        -> prPrec i 2 $ prt 0 str
    RDigit           -> ["$d"]
    RLetter          -> ["$l"]
    RUpper           -> ["$c"]