97132aa7ceaa9ec577255ad4acf5b9ef00514d6e
Andreas Abel <andreas.abel@ifi.lmu.de>
2020-10-26 12:38:45 +0100

    [ refactor for #265 ] doctests: don't rely on Library section in cabal file
    The include paths given to GHCI for running the doctests are more
    stable by using dist/build/bnfc-doctests/autogen/ instead of
    dist/build/autogen/ which is only present if BNFC is also a library.

--- source/test/doctests.hs
+++ source/test/doctests.hs
@@ -3,7 +3,7 @@ import Test.DocTest
 main = doctest
     [ "-icompat"
     , "-isrc"
-    , "-idist/build/autogen/"
+    , "-idist/build/bnfc-doctests/autogen/"
     , "-idist/build/bnfc/bnfc-tmp"
     , "-XLambdaCase"
     , "-XFlexibleContexts"
