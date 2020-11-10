981716694fb3dc8ded70067f6dd814696ae3296d
Frej Karlsson <frejlekarlsson@gmail.com>
2020-11-10 19:26:40 +0100

    Add newline to Parse error message in C++ backend
    There was a missing newline on the error message where the parsing failed

--- source/src/BNFC/Backend/CPP/STL.hs
+++ source/src/BNFC/Backend/CPP/STL.hs
@@ -144,7 +144,7 @@ cpptest inPackage cf =
     "  try { ",
     "  parse_tree = " ++ scope ++ "p" ++ def ++ "(input);",
     "  } catch( " ++ scope ++ "parse_error &e) {",
-    "     std::cerr << \"Parse error on line \" << e.getLine(); ",
+    "     std::cerr << \"Parse error on line \" << e.getLine() << \"\\n\"; ",
     "  }",
     "  if (parse_tree)",
     "  {",
