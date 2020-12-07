d11e3d57f061eb330c9d41cb02f48c2473f20bf2
Andreas Abel <andreas.abel@ifi.lmu.de>
2020-12-07 19:09:18 +0100

    [ doctests ] use </> for constructing paths

--- source/test/doctests.hs
+++ source/test/doctests.hs
@@ -1,16 +1,29 @@
-import Test.DocTest
+import System.FilePath ( (</>), joinPath )
+import Test.DocTest    ( doctest )
 
-main = doctest
-    [ "-icompat"
-    , "-isrc"
-    , "-idist/build/bnfc-doctests/autogen/"
-    , "-idist/build/bnfc/bnfc-tmp"
-    , "-XLambdaCase"
-    , "-XFlexibleContexts"
-    , "-XOverloadedStrings"
-    , "-XRecordWildCards"
-    , "-XScopedTypeVariables"
-    , "-XTupleSections"
-    , "src/PrintBNF.hs"
-    , "src/Main.hs"
+main = doctest $ concat
+  [ map ("-i" ++)
+    [ "src"
+    , joinPath [ "dist", "build", "bnfc-doctests", "autogen" ]
+    , joinPath [ "dist", "build", "bnfc", "bnfc-tmp" ]
     ]
+  , map ("-X" ++)
+    -- Keep the list of language extensions in sync with BNFC.cabal!
+    -- Keep in alphabetical order.
+    -- Exclude "CPP".
+    [ "DefaultSignatures"
+    , "DoAndIfThenElse"
+    , "FlexibleContexts"
+    , "FlexibleInstances"
+    , "LambdaCase"
+    , "PatternGuards"
+    , "OverloadedStrings"
+    , "RecordWildCards"
+    , "ScopedTypeVariables"
+    , "TupleSections"
+    ]
+  , map ("src" </>)
+    [ "PrintBNF.hs"
+    , "Main.hs"
+    ]
+  ]
