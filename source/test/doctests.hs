98f0d0bb3aeb617ecbf3923c11303ae9209fe897
Andreas Abel <andreas.abel@ifi.lmu.de>
2020-12-22 11:50:32 +0100

    [ cabal, doctests ] use cabal-doctest; package library BNFC
    The current doctests.hs module was a hack since it hard coded
    `dist`-paths in order to access the generated LexBNF.hs and ParBNF.hs.
    
    I instead now use the cabal-doctest package that generates a
    Build_doctests module with all the necessary information to invoke
    doctest.  This saves me from figuring out the paths to the
    cabal-generated files.
    
    To get BNFC compiled before doctests (which is necessary to have the
    Build_doctests module), I had to package BNFC into a library to depend
    on.  As a side benefit, the unit-tests can now depend on this library
    rather than recompiling all the modules.

--- source/test/doctests.hs
+++ source/test/doctests.hs
@@ -1,29 +1,35 @@
-import System.FilePath ( (</>), joinPath )
-import Test.DocTest    ( doctest )
+-- | @doctest@ main, from https://hackage.haskell.org/package/cabal-doctest
 
-main = doctest $ concat
-  [ map ("-i" ++)
-    [ "src"
-    , joinPath [ "dist", "build", "bnfc-doctests", "autogen" ]
-    , joinPath [ "dist", "build", "bnfc", "bnfc-tmp" ]
-    ]
-  , map ("-X" ++)
-    -- Keep the list of language extensions in sync with BNFC.cabal!
-    -- Keep in alphabetical order.
-    -- Exclude "CPP".
-    [ "DefaultSignatures"
-    , "DoAndIfThenElse"
-    , "FlexibleContexts"
-    , "FlexibleInstances"
-    , "LambdaCase"
-    , "PatternGuards"
-    , "OverloadedStrings"
-    , "RecordWildCards"
-    , "ScopedTypeVariables"
-    , "TupleSections"
-    ]
-  , map ("src" </>)
-    [ "PrintBNF.hs"
-    , "Main.hs"
-    ]
-  ]
+import Build_doctests      ( flags, pkgs, module_sources )
+import System.Environment  ( unsetEnv )
+import Test.DocTest        ( doctest )
+
+main :: IO ()
+main = do
+    -- optionally print arguments
+    mapM_ putStrLn . concat $
+      [ ["Arguments to doctest"]
+      , args
+      , [""]
+      ]
+    fixEnv
+    doctest args
+  where
+    args = flags ++ pkgs ++ module_sources
+
+-- | From version 2, Stack sets the GHC_ENVRIONMENT variable, and GHC
+-- (as invoked by doctest) will pick that up. This is undesirable:
+-- cabal-doctest passes all the necessary information on the command
+-- line already, and can lead to ambiguous module errors as GHC will
+-- load the environment in addition to what cabal-doctest instructs it
+-- to.
+--
+-- Hence, cabal-doctest tells GHC to ignore package environments
+-- altogether on the command line. However, this is only possible
+-- since GHC 8.2. If you are using cabal-doctest with Stack 2 and
+-- GHC 8.0 or earlier and seeing ambiguous module errors or other
+-- mysterious failures, try manually unsetting GHC_ENVIRONMENT before
+-- invoking doctest.
+--
+fixEnv :: IO ()
+fixEnv = unsetEnv "GHC_ENVIRONMENT"
