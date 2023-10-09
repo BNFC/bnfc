aee84f207634fe6686e660cd500f8c06aa979aad
Michael Dmitriev <m.dmitriev@innopolis.university>
2023-10-09 22:50:55 +0300

    [ANTLRv4] added generation of Makefile for ANTLR backend and several ANTLR targets

--- source/src/BNFC/Backend/Antlr.hs
+++ source/src/BNFC/Backend/Antlr.hs
@@ -3,7 +3,8 @@
 module BNFC.Backend.Antlr ( makeAntlr ) where
 
 import Prelude hiding ((<>))
-import System.FilePath ((</>), pathSeparator)
+import System.FilePath ((</>), pathSeparator, (<.>))
+import Text.PrettyPrint.HughesPJ (vcat)
 
 import BNFC.Utils
 import BNFC.CF
@@ -11,6 +12,7 @@ import BNFC.Options as Options
 import BNFC.Backend.Base
 import BNFC.Backend.Antlr.CFtoAntlr4Lexer
 import BNFC.Backend.Antlr.CFtoAntlr4Parser
+import BNFC.Backend.Common.Makefile as MakeFile
 
 makeAntlr :: SharedOptions -> CF -> MkFiles ()
 makeAntlr Options{..} cf = do
@@ -24,12 +26,72 @@ makeAntlr Options{..} cf = do
   --                                       +++ toolversion lexmake  ++ ")"
   let parserContent = parserFun packageBase cf linenumbers env
   mkfile (dirBase </> mkG4Name "Parser") mkAntlrComment parserContent
+
+  MakeFile.mkMakefile optMake makefileContent
     where
       lexerFun = cf2AntlrLex
       parserFun = cf2AntlrParse
       pkg = mkName [] CamelCase lang
       pkgToDir = replace '.' pathSeparator
-      mkG4Name name = lang ++ name ++ ".g4"
+      mkG4Name name = lang ++ name <.> "g4"
+
+      makeVars x = [MakeFile.mkVar n v | (n,v) <- x]
+      makeRules x = [MakeFile.mkRule tar dep recipe  | (tar, dep, recipe) <- x]
+
+      otherFlags = unwords $ getFlags [("no-listener", not listener), ("visitor", visitor), ("Werror", wError)]
+
+      langRef = MakeFile.refVar "LANG"
+
+      makefileVars = vcat $ makeVars
+        [ ("LANG", lang)
+        , ("LEXER_NAME", langRef ++ "Lexer")
+        , ("PARSER_NAME", langRef ++ "Parser")
+        , ("ANTLR4", "java org.antlr.v4.Tool")
+        , ("DLANGUAGE", parseAntlrTarget dLanguage)
+        , ("OTHER_FLAGS", otherFlags)
+        ]
+
+      refVarWithPrefix :: String -> String
+      refVarWithPrefix refVar = langRef </> MakeFile.refVar refVar
+
+      rmFile :: String -> String -> String
+      rmFile refVar ext = "rm -f" +++ refVarWithPrefix refVar ++ ext
+
+      makefileRules =  vcat $ makeRules
+        [ (".PHONY", ["all", "clean-g4", "remove"], [])
+        , ("all", [langRef], [])
+        , ("lexer", [refVarWithPrefix "LEXER_NAME" <.> "g4"], [MakeFile.refVar "ANTLR4" +++ "-Dlanguage=" ++ MakeFile.refVar "DLANGUAGE" +++ MakeFile.refVar "OTHER_FLAGS" +++ refVarWithPrefix "LEXER_NAME" <.> "g4"])
+        , ("parser", [refVarWithPrefix "PARSER_NAME" <.> "g4"], [MakeFile.refVar "ANTLR4" +++ "-Dlanguage=" ++ MakeFile.refVar "DLANGUAGE" +++ MakeFile.refVar "OTHER_FLAGS" +++ refVarWithPrefix "PARSER_NAME" <.> "g4"])
+        , (langRef, ["lexer", "parser"], [])
+        , ("clean-g4", [],
+          [ rmFile "LEXER_NAME" ".interp"
+          , rmFile "LEXER_NAME" ".tokens"
+          , rmFile "PARSER_NAME" ".interp"
+          , rmFile "PARSER_NAME" ".tokens"
+          ])
+        , ("remove", [], ["rm -rf" +++ langRef])
+        ]
+
+      makefileContent _ = vcat [makefileVars, "", makefileRules, ""]
 
 mkAntlrComment :: String -> String
 mkAntlrComment = ("// -*- ANTLRv4 -*- " ++)
+
+parseAntlrTarget :: AntlrTarget -> String
+parseAntlrTarget Java = "Java"
+parseAntlrTarget CPP = "Cpp"
+parseAntlrTarget CSharp = "CSharp"
+parseAntlrTarget JS = "JavaScript"
+parseAntlrTarget TS = "TypeScript"
+parseAntlrTarget Dart = "Dart"
+parseAntlrTarget Python3 = "Python3"
+parseAntlrTarget PHP = "PHP"
+parseAntlrTarget Go = "Go"
+parseAntlrTarget Swift = "Swift"
+
+getFlags :: [(String, Bool)] -> [String]
+getFlags (x : xs) = case x of
+  (flag, True) -> ("-" ++ flag) : getFlags xs
+  (_, False)   -> getFlags xs
+
+getFlags [] = []
--- source/src/BNFC/Options.hs
+++ source/src/BNFC/Options.hs
@@ -14,6 +14,7 @@ module BNFC.Options
   , AlexVersion(..), HappyMode(..), OCamlParser(..), JavaLexerParser(..)
   , RecordPositions(..), TokenText(..)
   , Ansi(..)
+  , AntlrTarget(..)
   , InPackage
   , removedIn290
   , translateOldOptions
@@ -81,7 +82,7 @@ instance Show Target where
   show TargetOCaml        = "OCaml"
   show TargetPygments     = "Pygments"
   show TargetCheck        = "Check LBNF file"
-  show TargetAntlr        = "Antlr4"
+  show TargetAntlr        = "ANTLRv4"
 
 -- | Which version of Alex is targeted?
 data AlexVersion = Alex3
@@ -109,6 +110,10 @@ data Ansi = Ansi | BeyondAnsi
 
 -- | Package name (C++ and Java backends).
 type InPackage = Maybe String
+
+-- | ANTLRv4 targets
+data AntlrTarget = CPP | CSharp | Dart | Java | JS | PHP | Python3 | Swift | TS | Go
+    deriving (Eq, Ord, Show)
 
 -- | How to represent token content in the Haskell backend?
 
@@ -146,6 +151,11 @@ data SharedOptions = Options
   --- C# specific
   , visualStudio  :: Bool        -- ^ Generate Visual Studio solution/project files.
   , wcf           :: Bool        -- ^ Windows Communication Foundation.
+  --- ANTLRv4 specific
+  , listener      :: Bool
+  , visitor       :: Bool
+  , wError        :: Bool
+  , dLanguage     :: AntlrTarget
   } deriving (Eq, Ord, Show)
 
 -- We take this opportunity to define the type of the backend functions.
@@ -180,6 +190,11 @@ defaultOptions = Options
   -- C# specific
   , visualStudio    = False
   , wcf             = False
+  -- ANTLRv4 specific
+  , listener        = True
+  , visitor         = False
+  , wError          = False
+  , dLanguage       = Java
   }
 
 -- | Check whether an option is unchanged from the default.
@@ -389,7 +404,32 @@ specificOptions =
   , ( Option []    ["agda"] (NoArg (\o -> o { agda = True, tokenText = TextToken }))
           "Also generate Agda bindings for the abstract syntax"
     , [TargetHaskell] )
+  , (Option  []    ["no-listener"] (NoArg (\o -> o { listener = False }))
+          "Generate visitor for ANTLR result"
+    ,  [TargetAntlr])
+  , (Option  []    ["visitor"] (NoArg (\o -> o { visitor = True }))
+          "Generate visitor for ANTLR result"
+    ,  [TargetAntlr])
+  , (Option  []    ["Werror"] (NoArg (\o -> o { wError = True }))
+          "Make ANTLR treat warnings as errors"
+    ,  [TargetAntlr])
+  , (Option  []    ["language"] (ReqArg (\lang o -> o {dLanguage = mkAntlrTarget lang}) "Dlanguage")
+          "Specify target language for ANTLR"
+    ,  [TargetAntlr])
   ]
+
+mkAntlrTarget :: String -> AntlrTarget
+mkAntlrTarget "java" = Java
+mkAntlrTarget "cpp" = CPP
+mkAntlrTarget "typescript" = TS
+mkAntlrTarget "javascript" = JS
+mkAntlrTarget "dart" = Dart
+mkAntlrTarget "go" = Go
+mkAntlrTarget "php" = PHP
+mkAntlrTarget "swift" = Swift
+mkAntlrTarget "python" = Python3
+mkAntlrTarget "csharp" = CSharp
+mkAntlrTarget _ = Java
 
 -- | The list of specific options for a target.
 specificOptions' :: Target -> [OptDescr (SharedOptions -> SharedOptions)]
@@ -451,7 +491,7 @@ help = unlines $ title ++
     , usageInfo "TARGET languages" targetOptions
     ] ++ map targetUsage helpTargets
   where
-  helpTargets = [ TargetHaskell, TargetJava, TargetC, TargetCpp ]
+  helpTargets = [ TargetHaskell, TargetJava, TargetC, TargetCpp, TargetAntlr ]
   targetUsage t = usageInfo
     (printf "Special options for the %s backend" (show t))
     (specificOptions' t)
