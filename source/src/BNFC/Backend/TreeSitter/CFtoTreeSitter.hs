c2ca38839139e20114fdb3134f4eaf8e98c54ff5
Kangjing Huang <huangkangjing@gmail.com>
2024-02-02 16:16:10 -0500

    Cleanup warning

--- source/src/BNFC/Backend/TreeSitter/CFtoTreeSitter.hs
+++ source/src/BNFC/Backend/TreeSitter/CFtoTreeSitter.hs
@@ -22,7 +22,7 @@ import BNFC.PrettyPrint
 
 import Prelude hiding ((<>))
 import qualified Data.Map as Map
-import Data.Map (Map)
+import Data.Map ()
 
 -- | Indent one level of 2 spaces
 indent :: Doc -> Doc
