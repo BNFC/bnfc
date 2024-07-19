34ada9bf84260ddff1629e43778a46d49cf177e7
Kangjing Huang <huangkangjing@gmail.com>
2024-07-19 00:02:48 -0400

    Revert "Try to put stack.yaml with a sub-dir"
    Does not work, attempting other workarounds.
    
    This reverts commit 45a860114ac05367226ed40187a52f654acb4c7c.

--- flake.nix
+++ flake.nix
@@ -18,17 +18,10 @@
         overlays = [ haskellNix.overlay
           (final: _prev: {
             bnfc-project =
-              final.haskell-nix.project' {
-                src = ./nix;
-                compiler-nix-name = "ghc982"; # Version of GHC to use
+              final.haskell-nix.hix.project {
+                src = ./.;
                 # uncomment with your current system for `nix flake show` to work:
                 evalSystem = "x86_64-linux";
-                # Tools to include in the development shell
-                shell.tools = {
-                  cabal = "latest";
-                  hlint = "latest";
-                  haskell-language-server = "lastest";
-                };
               };
           })
         ];
