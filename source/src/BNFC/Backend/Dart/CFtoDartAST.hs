f6fcda2588894b7e7c5360d35cd781b52043904c
xdkomel <xdghost.kamil@gmail.com>
2024-09-01 13:05:32 +0300

    mostly adequate version

--- source/src/BNFC/Backend/Dart/CFtoDartAST.hs
+++ source/src/BNFC/Backend/Dart/CFtoDartAST.hs
@@ -13,131 +13,127 @@ import BNFC.Backend.Common.NamedVariables ( UserDef )
 import BNFC.Backend.Dart.Common 
 
 -- Produces abstract data types in Dart
-cf2DartAST :: CF -> String
-cf2DartAST cf = 
+cf2DartAST :: String -> CF -> String
+cf2DartAST langName cf = 
   let userTokens = [ n | (n,_) <- tokenPragmas cf ]
-  in unlines $ 
-    imports ++  -- import some libraries if needed
-    generateTokens userTokens ++  -- generate user-defined types
-    concatMap prData rules
+  in unlines 
+    $ imports -- import some libraries if needed
+    ++ characterTypedef
+    ++ generateTokens userTokens 
+    ++ concatMap prData rules  -- generate user-defined types
   where
-  rules  = getAbstractSyntax cf
-  imports = [ 
-    "import 'pretty_printer.dart' as pp;",
-    "import 'package:fast_immutable_collections/fast_immutable_collections.dart';" ]
+    rules  = getAbstractSyntax cf
+    imports = 
+      [ "import 'pretty_printer.dart' as pp;"
+      , "import 'package:fast_immutable_collections/fast_immutable_collections.dart';" ]
+    characterTypedef = [ "typedef Character = String;" ]
+    censorName' = censorName langName
+    str2DartClassName' = str2DartClassName langName
+    cat2DartClassName' = cat2DartClassName langName
+    getVars' = getVars langName
 
 
-generateTokens :: [UserDef] -> [String]
-generateTokens tokens = map toClass tokens
-  where
-    toClass token = 
-      let name = censorName token
-      in unlines [
-        "final class" +++ name +++ "with pp.Printable {",  -- A user defined type is a wrapper around the String
-        "  final String value;",
-        "  const" +++ name ++ "(this.value);",
-        "",
-        "  @override",
-        "  String get print => pp.print" ++ name ++ "(this);",
-        "}"
-      ]
+    generateTokens :: [UserDef] -> [String]
+    generateTokens = map $ \token -> 
+        let name = censorName' token 
+        in "typedef" +++ name +++ "= String;"
+          
+
+    -- | Generates a category class, and classes for all its rules.
+    prData :: Data -> [String]
+    prData (cat, rules) =
+      categoryClass ++ mapMaybe (prRule cat) rules
+        where
+        funs = map fst rules
+        categoryClass
+          | catToStr cat `elem` funs || isList cat = [] -- the category is also a function or a list
+          | otherwise =
+            let name = cat2DartClassName' cat
+            in 
+              [ "sealed class" +++ name +++ "with pp.Printable {"
+              , "  @override"
+              , "  String get print => pp.print" ++ name ++ "(this);"
+              , "}" ]
 
 
--- | Generates a category class, and classes for all its rules.
-prData :: Data -> [String]
-prData (cat, rules) =
-  categoryClass ++ mapMaybe (prRule cat) rules
-    where
-    funs = map fst rules
-    categoryClass
-      | catToStr cat `elem` funs || isList cat = [] -- the category is also a function or a list
-      | otherwise =
-        let name = cat2DartClassName cat
-        in [ 
-          "sealed class" +++ name +++ "with pp.Printable {",
-          "  @override",
-          "  String get print => pp.print" ++ name ++ "(this);",
-          "}" ]
+    -- | Generates classes for a rule, depending on what type of rule it is.
+    prRule :: Cat -> (Fun, [Cat]) -> Maybe (String)
+    prRule cat (fun, cats)
+      | isNilFun fun || 
+        isOneFun fun || 
+        isConsFun fun = Nothing  -- these are not represented in the Absyn
+      | otherwise = -- a standard rule
+        let 
+          className = str2DartClassName' fun
+          vars = getVars' cats
+        in Just . unlines $ 
+          [ unwords [ "class", className, extending, "with pp.Printable {" ] ] ++
+          concatMap (indent 1) [
+            prInstanceVariables vars,
+            prConstructor className vars,
+            prEquals className vars,
+            prHashCode vars,
+            prPrettyPrint className
+          ] ++ [ "}" ] 
+      where
+        extending 
+          | fun == catToStr cat = ""
+          | otherwise = "extends" +++ cat2DartClassName' cat
 
 
--- | Generates classes for a rule, depending on what type of rule it is.
-prRule :: Cat -> (Fun, [Cat]) -> Maybe (String)
-prRule cat (fun, cats)
-  | isNilFun fun || 
-    isOneFun fun || 
-    isConsFun fun = Nothing  -- these are not represented in the Absyn
-  | otherwise = -- a standard rule
-    let 
-      className = str2DartClassName fun
-      vars = getVars cats
-    in Just . unlines $ 
-      [ unwords [ "class", className, extending, "with pp.Printable {" ] ] ++
-      concatMap (indent 1) [
-        prInstanceVariables vars,
-        prConstructor className vars,
-        prEquals className vars,
-        prHashCode vars,
-        prPrettyPrint className
-      ] ++ [ "}" ] 
-  where
-    extending 
-      | fun == catToStr cat = ""
-      | otherwise = "extends" +++ cat2DartClassName cat
+    -- Override the equality `==`
+    prEquals :: String -> [DartVar] -> [String]
+    prEquals className variables = [
+        "@override",
+        "bool operator ==(Object o) =>",
+        "  o is" +++ className +++ "&&",
+        "  o.runtimeType == runtimeType" ++ 
+          (if null variables then ";" else " &&")
+      ] ++ checkChildren
+      where 
+        checkChildren = generateEqualities variables
+        generateEqualities [] = []
+        generateEqualities (variable:rest) = 
+          let name = buildVariableName variable 
+          in [ 
+            "  " ++ name +++ "==" +++ "o." ++ name ++
+              (if null rest then ";" else " &&") 
+          ] ++ generateEqualities rest
 
 
--- Override the equality `==`
-prEquals :: String -> [DartVar] -> [String]
-prEquals className variables = [
-    "@override",
-    "bool operator ==(Object o) =>",
-    "  o is" +++ className +++ "&&",
-    "  o.runtimeType == runtimeType" ++ 
-      (if null variables then ";" else " &&")
-  ] ++ checkChildren
-  where 
-    checkChildren = generateEqualities variables
-    generateEqualities [] = []
-    generateEqualities (variable:rest) = 
-      let name = buildVariableName variable 
-      in [ 
-        "  " ++ name +++ "==" +++ "o." ++ name ++
-          (if null rest then ";" else " &&") 
-      ] ++ generateEqualities rest
+    -- Override the hashCode, combining all instance variables
+    prHashCode :: [DartVar] -> [String]
+    prHashCode vars = [
+        "@override",
+        "int get hashCode => Object.hashAll([" ++ 
+          concatMap variableHash vars ++ 
+          "]);"
+      ] 
+      where
+        variableHash variable = buildVariableName variable ++ ", "
 
 
--- Override the hashCode, combining all instance variables
-prHashCode :: [DartVar] -> [String]
-prHashCode vars = [
-    "@override",
-    "int get hashCode => Object.hashAll([" ++ 
-      concatMap variableHash vars ++ 
-      "]);"
-  ] 
-  where
-    variableHash variable = buildVariableName variable ++ ", "
+    -- Generate variable definitions for the class
+    prInstanceVariables :: [DartVar] -> [String]
+    prInstanceVariables vars = map variableLine vars
+      where
+        variableLine variable =
+          let vType = buildVariableType variable
+              vName = buildVariableName variable
+          in "final" +++ vType +++ vName ++ ";"
+          
 
+    -- Generate the class constructor
+    prConstructor :: String -> [DartVar] -> [String]
+    prConstructor className vars = 
+      [ className ++ "(" ++ variablesAssignment ++ ");" ]
+      where 
+        variablesAssignment
+          | null vars = ""
+          | otherwise = "{" ++ (concatMap assignment vars) ++ "}"
+        assignment variable = "required this." ++ buildVariableName variable ++ ", "
 
--- Generate variable definitions for the class
-prInstanceVariables :: [DartVar] -> [String]
-prInstanceVariables vars = map variableLine vars
-  where
-    variableLine variable =
-      let vType = buildVariableType variable
-          vName = buildVariableName variable
-      in "final" +++ vType +++ vName ++ ";"
-       
-
--- Generate the class constructor
-prConstructor :: String -> [DartVar] -> [String]
-prConstructor className vars = 
-  [ className ++ "(" ++ variablesAssignment ++ ");" ]
-  where 
-    variablesAssignment
-      | null vars = ""
-      | otherwise = "{" ++ (concatMap assignment vars) ++ "}"
-    assignment variable = "required this." ++ buildVariableName variable ++ ", "
-
-prPrettyPrint :: String -> [String]
-prPrettyPrint name = [
-  "@override",
-  "String get print => pp.print" ++ name ++ "(this);" ]
+    prPrettyPrint :: String -> [String]
+    prPrettyPrint name = [
+      "@override",
+      "String get print => pp.print" ++ name ++ "(this);" ]
--- source/src/BNFC/Backend/Dart/Common.hs
+++ source/src/BNFC/Backend/Dart/Common.hs
@@ -6,16 +6,17 @@ module BNFC.Backend.Dart.Common where
 
 import qualified Data.Map as Map
 import BNFC.CF
+import Data.Maybe
 import qualified Data.Char as Char
 
 
-cat2DartClassName :: Cat -> String
-cat2DartClassName cat = str2DartClassName $ identCat $ normCat cat
+cat2DartClassName :: String -> Cat -> String
+cat2DartClassName langName cat = str2DartClassName langName $ identCat $ normCat cat
 
 
 -- Pick a class name that is appropriate for the Dart
-str2DartClassName :: String -> String
-str2DartClassName str = upperFirst $ censorName str
+str2DartClassName :: String -> String -> String
+str2DartClassName langName str = upperFirst $ censorName langName str
 
 
 -- Pick a class name that is appropriate for the Antlr
@@ -23,28 +24,35 @@ str2AntlrClassName :: String -> String
 str2AntlrClassName str = upperFirst str
 
 
-cat2DartType :: Cat -> DartVarType
-cat2DartType cat = toList (0, cat)
+cat2DartType :: String -> Cat -> DartVarType
+cat2DartType langName cat = toList (0, cat)
   where
     toList :: (Int, Cat) -> DartVarType
     toList (n, (ListCat name)) = toList (n + 1, name)
-    toList (n, name) = (n, (name2DartBuiltIn $ catToStr $ normCat name))
+    toList (n, name) = 
+      ( n
+      , let n = catToStr $ normCat name
+        in case (name2DartBuiltIn n) of 
+          Just bn -> bn
+          Nothing -> censor n )
+    censor = censorName langName
 
 
-cat2DartName :: Cat -> String
-cat2DartName cat = toList $ normCat cat
+cat2DartName :: String -> Cat -> String
+cat2DartName langName cat = toList $ normCat cat
   where
     toList (ListCat name) = toList name ++ "List"
-    toList name = censorName $ catToStr name
+    toList name = censorName langName $ catToStr name
 
 
-name2DartBuiltIn :: String -> String
+name2DartBuiltIn :: String -> Maybe String
 name2DartBuiltIn name
-  | name == "Integer" = "int"
-  | name == "Double" = "double"
-  | name == "Ident" = "String"
-  | name == "Char" = "String"
-  | otherwise = name
+  | name == "Integer" = Just "int"
+  | name == "Double" = Just "double"
+  | name == "Ident" = Just "String"
+  | name == "String" = Just "String"
+  | name == "Char" = Just "Character"
+  | otherwise = Nothing
 
 
 upperFirst :: [Char] -> [Char]
@@ -83,32 +91,34 @@ type DartVarName = (String, Int)
 
 
 -- Because of the different type representing variables, a different `getVars` is used.
-getVars :: [Cat] -> [DartVar]
-getVars cats = 
+getVars :: String -> [Cat] -> [DartVar]
+getVars langName cats = 
   let variables = map toUnnamedVariable cats 
       namesMap = foldl countNames Map.empty variables
       scoreMap = Map.map addScore namesMap
       (_, vars) = foldl toDartVar (scoreMap, []) variables
   in vars
-  where
-    toUnnamedVariable cat = ((cat2DartType cat), (cat2DartName cat))
-    countNames namesMap (_, name) = 
-      let current = Map.findWithDefault 0 name namesMap
-          next = 1 + current
-      in Map.insert name next namesMap
-    addScore n = (1, n)
-    toDartVar (namesMap, vars) (vType, name) =
-      case (Map.lookup name namesMap) of
-        Nothing -> (
-          namesMap, 
-          vars ++ [(vType, (name, 0))])
-        Just (seen, total) -> if total <= 1 
-          then (
+    where
+      cat2DartName' = cat2DartName langName
+      cat2DartType' = cat2DartType langName
+      toUnnamedVariable cat = ((cat2DartType' cat), (cat2DartName' cat))
+      countNames namesMap (_, name) = 
+        let current = Map.findWithDefault 0 name namesMap
+            next = 1 + current
+        in Map.insert name next namesMap
+      addScore n = (1, n)
+      toDartVar (namesMap, vars) (vType, name) =
+        case (Map.lookup name namesMap) of
+          Nothing -> (
             namesMap, 
             vars ++ [(vType, (name, 0))])
-          else (
-            Map.insert name (seen + 1, total) namesMap, 
-            vars ++ [(vType, (name, seen))])
+          Just (seen, total) -> if total <= 1 
+            then (
+              namesMap, 
+              vars ++ [(vType, (name, 0))])
+            else (
+              Map.insert name (seen + 1, total) namesMap, 
+              vars ++ [(vType, (name, seen))])
 
 
 -- From a DartVar build its string representation
@@ -131,13 +141,114 @@ buildVariableTypeFromDartType vType = unpack vType
     unpack (n, name) = "IList<" ++ unpack (n - 1, name) ++ ">"
 
 
--- Prevent some type or variable name to be called as some built-in Dart type
-censorName :: String -> String
-censorName name 
-  | (lowerFirst name) `elem` (map lowerFirst builtIn) = "My" ++ upperFirst name
+checkBuiltIn :: String -> Bool
+checkBuiltIn name = 
+  (lowerFirst name) `elem` concatMap 
+      (map lowerFirst) 
+      [ builtIn, keywords ]
+
+
+checkRegistered :: String -> Bool
+checkRegistered name = 
+  (lowerFirst name) `elem` concatMap 
+      (map lowerFirst) 
+      [ builtIn, keywords, taken ]
+
+
+-- Prevent some type or variable name to be called as some already used type or keyword
+censorName :: String -> String -> String
+censorName langName name 
+  | checkRegistered name = langName ++ upperFirst name
   | otherwise = name
-  where
-    builtIn = [ "int", "double", "String", "bool", "List", "Set", "Map", 
-      "Runes", "Symbol", "Record", "Future", "null", "Null", "if", "else", 
-      "return", "throw", "try", "catch", "on", "switch", "var", "final", "sync", 
-      "async", "for", "while", "continue", "break" ]
+
+taken = [ "Character" ]
+
+builtIn = [ "int"
+          , "double"
+          , "num"
+          , "String"
+          , "bool"
+          , "List"
+          , "Set"
+          , "Map"
+          , "Runes"
+          , "Symbol"
+          , "null"
+          , "Null"
+          , "Object"
+          , "Enum"
+          , "Future"
+          , "Stream"
+          , "Iterable"
+          , "Never"
+          , "dynamic"
+          , "void" ]
+
+keywords = [ "abstract"
+          , "as"
+          , "assert"
+          , "async"
+          , "await"
+          , "base"
+          , "break"
+          , "case"
+          , "catch"
+          , "class"
+          , "const"
+          , "continue"
+          , "covariant"
+          , "default"
+          , "deferred"
+          , "do"
+          , "dynamic"
+          , "else"
+          , "enum"
+          , "export"
+          , "extends"
+          , "extension"
+          , "external"
+          , "factory"
+          , "false"
+          , "final"
+          , "finally"
+          , "for"
+          , "Function"
+          , "get"
+          , "hide"
+          , "if"
+          , "implements"
+          , "import"
+          , "in"
+          , "interface"
+          , "is"
+          , "late"
+          , "library"
+          , "mixin"
+          , "new"
+          , "null"
+          , "of"
+          , "on"
+          , "operator"
+          , "part"
+          , "required"
+          , "rethrow"
+          , "return"
+          , "sealed"
+          , "set"
+          , "show"
+          , "static"
+          , "super"
+          , "switch"
+          , "sync"
+          , "this"
+          , "throw"
+          , "true"
+          , "try"
+          , "type"
+          , "typedef"
+          , "var"
+          , "void"
+          , "when"
+          , "with"
+          , "while"
+          , "yield" ]
