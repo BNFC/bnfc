73101b09cf337e8269e89f618d90fb3d713f378f
Juan Pablo <juanpaps03@gmail.com>
2024-10-10 21:08:06 +0200

    Update Abs to be more Scala ish

--- source/src/BNFC/Backend/Scala/CFtoScalaAbs.hs
+++ source/src/BNFC/Backend/Scala/CFtoScalaAbs.hs
@@ -23,6 +23,7 @@ import BNFC.PrettyPrint
 import BNFC.Backend.Haskell.Utils
 import Data.Either
 import BNFC.Options
+import Data.List (intercalate)
 
 -- | The result is two files (.H file, .C file)
 cf2ScalaAbs
@@ -44,17 +45,20 @@ cf2ScalaAbs Options{ lang, tokenText, generic, functor } cf = vsep . concat $
 dataToString :: (Cat, [(String, [Cat])]) -> String
 dataToString (cat, _) = catToStr cat
 
+mapBut1 :: (a -> a) -> [a] -> [a]
+mapBut1 f (x:y:xs) = f x : mapBut1 f (y:xs)
+mapBut1 _ other = other
 
 prData :: Bool -> Data -> Doc
 prData functor (cat,rules) = vcat $ concat
   [ 
-    [ hang ("Sclass" <+> dataType) 4 $ constructors rules ]
+    [ hang ("Sclass" <+> dataType <+> ":") 4 $ constructors rules ] -- I have to find a way to add strings without space, <+> add stirngs but add also a space
   ]
   where
-    prRule (fun, cats)   = hsep $ concat [ [text fun], map prArg cats ]
+    -- This "mapBut1" is used to add a comma between numerated parameters, but I think should be a better way
+    prRule (fun, cats)   = hsep $ concat [ [text fun <+> "("], mapBut1 (<+> text ", ") (map prArg (indexedCats cats)) , [text ") {}"] ]
     dataType             = pretty cat
-    prArg c              = catToType id empty c
+    indexedCats c        = zip [0..] c
+    prArg (n, c)         = text $ catToStr c ++ " " ++ ("p" ++ show n) 
     constructors []      = empty
-    -- I need to fix the next line, is adding the : at the haskell style and not Scala
-    constructors (h:t)   = sep $ [prRule h <+> ":"] ++ map ((<+>":") . prRule) t  -- TODO: this can be improved, there is no need for 2 maps
-    -- constructors (h:t)   = sep $ [text h <+> ":"]  
+    constructors (h:t)   = sep $ [prRule h] ++ map prRule t
