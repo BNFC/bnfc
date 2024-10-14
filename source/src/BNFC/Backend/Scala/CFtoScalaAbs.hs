5900317ffce607fae15408abb3551ed5ca35af96
Juan Pablo <juanpaps03@gmail.com>
2024-10-14 21:06:59 +0200

    Change Abs to match Scala syntax example

--- source/src/BNFC/Backend/Scala/CFtoScalaAbs.hs
+++ source/src/BNFC/Backend/Scala/CFtoScalaAbs.hs
@@ -20,45 +20,46 @@ import Prelude hiding ((<>))
 
 import BNFC.CF
 import BNFC.PrettyPrint
-import BNFC.Backend.Haskell.Utils
-import Data.Either
 import BNFC.Options
-import Data.List (intercalate)
 
 -- | The result is two files (.H file, .C file)
 cf2ScalaAbs
   :: SharedOptions     
   -> CF     -- ^ Grammar.
   -> Doc    -- ^ @.H@ file, @.C@ file.
-cf2ScalaAbs Options{ lang, tokenText, generic, functor } cf = vsep . concat $
+cf2ScalaAbs Options{ lang } cf = vsep . concat $
   [ 
       []
-    , map text $ map dataToString datas
-    , map (prData functor) datas
-    , ["// amount of datas: ", text $ show $ length datas]
-    , ["// ScalaAbsFile", ""]
+    , ["package" <+> text lang]
+    , [hang classSign 4 $ functions]
+    , defaultFunction lang
+    , ["}"]
   ]
   where
-    datas    = cf2data cf
+    datas     = cf2data cf
+    functions = vcat (map prData datas)
 
 
-dataToString :: (Cat, [(String, [Cat])]) -> String
-dataToString (cat, _) = catToStr cat
+-- dataToString :: (Cat, [(String, [Cat])]) -> String
+-- dataToString (cat, _) = catToStr cat
 
-mapBut1 :: (a -> a) -> [a] -> [a]
-mapBut1 f (x:y:xs) = f x : mapBut1 f (y:xs)
-mapBut1 _ other = other
+classSign :: Doc
+classSign = "abstract class AbstractVisitor[R, A] extends AllVisitor[R, A]{"
 
-prData :: Bool -> Data -> Doc
-prData functor (cat,rules) = vcat $ concat
+prData ::  Data -> Doc
+prData (_, rules) = vcat $ concat
   [ 
-    [ hang ("Sclass" <+> dataType <+> ":") 4 $ constructors rules ] -- I have to find a way to add strings without space, <+> add stirngs but add also a space
+    [constructors rules]
   ]
   where
-    -- This "mapBut1" is used to add a comma between numerated parameters, but I think should be a better way
-    prRule (fun, cats)   = hsep $ concat [ [text fun <+> "("], mapBut1 (<+> text ", ") (map prArg (indexedCats cats)) , [text ") {}"] ]
-    dataType             = pretty cat
-    indexedCats c        = zip [0..] c
-    prArg (n, c)         = text $ catToStr c ++ " " ++ ("p" ++ show n) 
+    prRule (fun, _)   = hsep $ concat [ ["def visit(p: " <+> text ("Absyn." ++ fun) <+> text ", arg: A): R = visitDefault(p, arg)"] ]
     constructors []      = empty
     constructors (h:t)   = sep $ [prRule h] ++ map prRule t
+  
+defaultFunction :: String -> [Doc]
+defaultFunction lang = 
+  [
+      nest 4 $ text $ "def visitDefault(p: Absyn." ++ lang ++ ", arg: A): R = {"
+    , nest 8 "throw new IllegalArgumentException(this.getClass.getName + \": \" + p)"
+    , nest 4 "}"
+  ]
