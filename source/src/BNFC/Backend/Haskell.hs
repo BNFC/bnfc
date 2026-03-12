26d9f461f62811756d9ecff905fc7fd74ce0648d
Commelina <maosics@gmail.com>
2026-03-12 02:13:44 +0100

    Haskell: add `--positions=range` flag to include start and end position

--- source/src/BNFC/Backend/Haskell.hs
+++ source/src/BNFC/Backend/Haskell.hs
@@ -54,27 +54,27 @@ makeHaskell opts cf = do
   do
     -- Generate abstract syntax and pretty printer.
     mkfile (absFile opts) comment $ cf2Abstract opts absMod cf
-    mkfile (printerFile opts) comment $ cf2Printer (tokenText opts) (functor opts) False prMod absMod cf
+    mkfile (printerFile opts) comment $ cf2Printer (tokenText opts) (hasNotNonePos . positions $ opts) False prMod absMod cf
 
     -- Generate Alex lexer.  Layout is resolved after lexing.
     case alexMode opts of
       Alex3 -> do
-        mkfile (alexFile opts) commentWithEmacsModeHint $ cf2alex3 lexMod (tokenText opts) cf
+        mkfile (alexFile opts) commentWithEmacsModeHint $ cf2alex3 lexMod (tokenText opts) (positions opts) cf
         liftIO $ printf "Use Alex 3 to compile %s.\n" (alexFile opts)
 
     Ctrl.when (hasLayout cf) $ mkfile (layoutFile opts) comment $
-      cf2Layout layMod lexMod cf
+      cf2Layout layMod lexMod (positions opts) cf
 
     -- Generate Happy parser and matching test program.
     do
       mkfile (happyFile opts) commentWithEmacsModeHint $
-        cf2Happy parMod absMod lexMod (glr opts) (tokenText opts) (functor opts) cf
+        cf2Happy parMod absMod lexMod (glr opts) (tokenText opts) (positions opts) cf
       -- liftIO $ printf "%s Tested with Happy 1.15\n" (happyFile opts)
       mkfile (tFile opts) comment $ testfile opts cf
 
     -- Both Happy parser and skeleton (template) rely on Err.
     mkfile (errFile opts) comment $ mkErrM errMod
-    mkfile (templateFile opts) comment $ cf2Template (templateFileM opts) absMod (functor opts) cf
+    mkfile (templateFile opts) comment $ cf2Template (templateFileM opts) absMod (hasNotNonePos . positions $ opts) cf
 
     -- Generate txt2tags documentation.
     mkfile (txtFile opts) t2tComment $ cfToTxt (lang opts) cf
@@ -90,7 +90,6 @@ makeHaskell opts cf = do
 
     -- Generate Makefile.
     Makefile.mkMakefile (optMake opts) $ makefile opts cf
-
 
 -- | Generate the makefile (old version, with just one "all" target).
 _oldMakefile
