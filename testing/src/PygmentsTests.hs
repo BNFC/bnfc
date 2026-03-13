da8663fc92a60e4054ce1c3909f0697284be1edb
Commelina <maosics@gmail.com>
2026-03-13 17:08:55 +0100

    [ testing ] add a pygments testcase for special symbols `^` and `$`

--- testing/src/PygmentsTests.hs
+++ testing/src/PygmentsTests.hs
@@ -4,7 +4,12 @@ import TestUtils
 import Shelly
 
 all = makeTestSuite "Pygments backend"
-    [ testPygmentsCalc ]
+    [ testPygmentsCalc
+    , testPygmentsSpecialSymbol
+    ]
+
+-- TODO: Extract common code as utils
+-- FIXME: Choose a better location for test data
 
 testPygmentsCalc :: Test
 testPygmentsCalc =
@@ -25,3 +30,23 @@ testPygmentsCalc =
             readfile "pygments-calc-input.txt" >>= setStdin
             output <- cmd (env </> "bin/pygmentize") "-l" "calc" "-fraw"
             assertEqual expected output
+
+testPygmentsSpecialSymbol :: Test
+testPygmentsSpecialSymbol =
+  makeShellyTest "#477 Pygments backend is generating regex with unescaped symbol (^ and $)" $
+    withTmpDir $ \tmp -> do
+      cp "regression-tests/477_PygmentsSpecialSymbols/Symbols.cf" tmp
+      cp "regression-tests/477_PygmentsSpecialSymbols/pygments-symbols-input.txt" tmp
+      cp "regression-tests/477_PygmentsSpecialSymbols/pygments-symbols-output.txt" tmp
+      cd tmp
+      cmd "bnfc" "--pygments" "Symbols.cf"
+      assertFileExists "setup.py"
+      assertFileExists "symbols/__init__.py"
+      cmd "python3" "-m" "venv" "env"
+      env <- canonicalize "./env/"
+      cmd (env </> "bin/python") "setup.py" "install"
+      assertFileExists (env </> "bin/pygmentize")
+      expected <- readfile "pygments-symbols-output.txt"
+      readfile "pygments-symbols-input.txt" >>= setStdin
+      output <- cmd (env </> "bin/pygmentize") "-l" "symbols" "-fraw"
+      assertEqual expected output
